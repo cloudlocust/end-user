42f4bc408ab90fc2fcad4bf4e7d8e3ddb0e116ed
kseniabezugla <kseniia@myem.fr>
2022-05-12 10:58:29 +0200

    MYEM-2227 #time 50m  #comment Added tests

---
+++ src/modules/Profile/NumberField.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { reduxedRender } from 'src/common/react-platform-components/test'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { fireEvent, act } from '@testing-library/react'
+import { NumberField } from './NumberField'
+
+// Text variables.
+const labelTitle = 'PC de bureau'
+
+const propsNumberField = { title: labelTitle, iconLabel: 'computer', disableDecrement: true, initialCount: 1 }
+
+describe('NumberField Test', () => {
+    describe('load NumberField', () => {
+        test('on success loading the element, NumberField should be loaded, title shown', async () => {
+            const { getByText } = reduxedRender(
+                <Router>
+                    <NumberField {...propsNumberField} />
+                </Router>,
+            )
+            expect(getByText(labelTitle)).toBeTruthy()
+        })
+    })
+    describe('Buttons test', () => {
+        test('Increment test', async () => {
+            const { getByText } = reduxedRender(
+                <Router>
+                    <NumberField {...propsNumberField} />
+                </Router>,
+            )
+            act(() => {
+                fireEvent.click(getByText('add'))
+            })
+            expect(getByText(2)).toBeTruthy()
+        })
+        test('Decrement test', async () => {
+            const { getByText } = reduxedRender(
+                <Router>
+                    <NumberField {...propsNumberField} />
+                </Router>,
+            )
+            act(() => {
+                fireEvent.click(getByText('remove'))
+            })
+            expect(getByText(0)).toBeTruthy()
+        })
+    })
+})
