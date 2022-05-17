b57501529e8cc451f69bdd595db1c7ecae2dec86
kseniabezugla <kseniia@myem.fr>
2022-05-17 16:36:11 +0200

    MYEM-2358 #time 32m  #comment Added tests

---
+++ src/common/ui-kit/components/SelectButtons/SelectButtons.test.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Form } from 'src/common/react-platform-components'
+import { reduxedRender } from 'src/common/react-platform-components/test'
+import { fireEvent, act } from '@testing-library/react'
+import { SelectButtons } from './SelectButtons'
+
+const propsSelectButtons = {
+    titleLabel: 'Type de plaques de cuisson:',
+    name: 'hotplates',
+    value: 'other',
+    formOptions: [
+        {
+            iconLabel: 'remove',
+            label: 'electricity',
+            buttonStyle: 'mt-16 flex flex-col mr-10',
+            isDisabled: false,
+        },
+        {
+            iconPath: '/assets/images/content/equipment/heatingOther.svg',
+            label: 'induction',
+            buttonStyle: 'mt-16 flex flex-col mr-10',
+            isDisabled: false,
+        },
+        {
+            label: 'other',
+            buttonStyle: 'mt-16 flex flex-col',
+            isDisabled: false,
+        },
+    ],
+}
+
+describe('<SelectButtons /> props', () => {
+    test('props must be passed to SelectButtons and value shown', () => {
+        const { getByText } = reduxedRender(
+            <Form onSubmit={() => {}}>
+                <SelectButtons {...propsSelectButtons} />
+            </Form>,
+        )
+        expect(getByText('Type de plaques de cuisson:')).toBeTruthy()
+    })
+    test('should use correctly by SelectButtons', async () => {
+        const handleSubmit = jest.fn()
+        const { getByTestId } = reduxedRender(
+            // eslint-disable-next-line jsdoc/require-jsdoc
+            <Form onSubmit={(data) => handleSubmit(data)}>
+                <SelectButtons {...propsSelectButtons} />
+                <input type="submit" data-testid="submit" />
+            </Form>,
+        )
+        await act(async () => {
+            userEvent.click(getByTestId('submit'))
+        })
+        expect(handleSubmit).toHaveBeenCalledWith({ hotplates: 'other' })
+    })
+    test('value should be changed', async () => {
+        const handleSubmit = jest.fn()
+        const { getByText, getByTestId } = reduxedRender(
+            // eslint-disable-next-line jsdoc/require-jsdoc
+            <Form onSubmit={(data) => handleSubmit(data)}>
+                <SelectButtons {...propsSelectButtons} />
+                <input type="submit" data-testid="submit" />
+            </Form>,
+        )
+        await act(async () => {
+            fireEvent.click(getByText('induction'))
+        })
+        await waitFor(async () => {
+            expect(getByText('induction').getAttribute('value')).toBe('induction')
+        })
+        act(() => {
+            fireEvent.click(getByTestId('submit'))
+        })
+        await waitFor(async () => {
+            expect(handleSubmit).toHaveBeenCalledWith({ hotplates: 'induction' })
+        })
+    })
+})
