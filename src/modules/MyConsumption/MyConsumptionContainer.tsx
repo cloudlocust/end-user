29304b7932638f095f875e47a5f98d218645e166
MediMESS <mehdi@myem.fr>
2022-06-12 18:41:44 +0100

    MYEM-2422 #time 2m #comment Fix styling in MyConsumptionContainer

--- src/modules/MyConsumption/MyConsumptionContainer.tsx
+++ src/modules/MyConsumption/MyConsumptionContainer.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { MyConsumptionChart } from 'src/modules/MyConsumption'
 import { MyConsumptionSelectMeters } from 'src/modules/MyConsumption/components/MyConsumptionSelectMeters'
-import { MyConsumptionPeriod } from './components/MyConsumptionPeriod/MyConsumptionPeriod'
 import { useConsumptionMetrics } from 'src/modules/Metrics/metricsHook'
 import { getMetricType } from 'src/modules/Metrics/Metrics'
 import dayjs from 'dayjs'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
+import { useMeterList } from 'src/modules/Meters/metersHook'
+import { MyConsumptionPeriod } from 'src/modules/MyConsumption/components/MyConsumptionPeriod/MyConsumptionPeriod'
+import MyConsumptionChart from './components/MyConsumptionChart/MyConsumptionChart'
 import { periodValue } from 'src/modules/MyConsumption/myConsumptionTypes'
+import { useTheme } from '@mui/material'
 
 /**
- * InitialMetricsStates for useConsumptionMetrics.
+ * InitialMetricsStates for useMetrics.
  */
 export const initialMetricsHookValues: getMetricType = {
     interval: '1min',
@@ -36,6 +38,8 @@ export const MyConsumptionContainer = () => {
     const { setPeriod, setRange, setFilters, isMetricsLoading, data, interval } =
         useConsumptionMetrics(initialMetricsHookValues)
     const [periodValue, setPeriodValue] = useState<periodValue>(1)
+    const { elementList: metersList } = useMeterList()
+    const theme = useTheme()
 
     /**
      * Show text according to interval.
@@ -57,20 +61,28 @@ export const MyConsumptionContainer = () => {
     }
 
     return (
-        <>
-            <div className="container relative p-16 sm:p-24 flex flex-col sm:flex-row justify-between items-center">
-                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                    <div className="flex flex-col sm:flex-row items-center sm:items-center mb-16 sm:mb-0 ">
-                        <TypographyFormatMessage className="h3 sm:mr-3" color="textPrimary">
+        <div style={{ background: theme.palette.primary.main }} className="p-24">
+            <div className="relative flex flex-col md:flex-row justify-between items-center">
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="mb-16 md:mb-0">
+                    <div className="flex flex-col md:flex-row items-center">
+                        <TypographyFormatMessage
+                            variant="h4"
+                            className="sm:mr-8"
+                            style={{ color: theme.palette.primary.contrastText }}
+                        >
                             Ma Consommation
                         </TypographyFormatMessage>
-                        <div className="flex flex-row">
+                        <div className="flex flex-row items-end">
                             {/* TODO: kWh can also be P.max in MYEM-2408, to be dynamic. */}
-                            <TypographyFormatMessage className="h3 mr-3 sm:mr-3" color="textSecondary">
+                            <TypographyFormatMessage
+                                variant="h5"
+                                className="mr-8 sm:mr-8"
+                                style={{ color: theme.palette.primary.contrastText }}
+                            >
                                 en kWh
                             </TypographyFormatMessage>
                             {/* Consommation par Jour / Semaiine / Mois / Année */}
-                            <TypographyFormatMessage className="h3" color="textSecondary">
+                            <TypographyFormatMessage variant="h5" style={{ color: theme.palette.primary.contrastText }}>
                                 {showPerPeriodText()}
                             </TypographyFormatMessage>
                         </div>
@@ -78,9 +90,11 @@ export const MyConsumptionContainer = () => {
                 </motion.div>
 
                 {/* TODO: MYEM-2418 */}
-                <MyConsumptionSelectMeters setFilters={setFilters} />
+                {metersList && metersList?.length > 1 && (
+                    <MyConsumptionSelectMeters metersList={metersList} setFilters={setFilters} />
+                )}
             </div>
-            {/* TODO: MYEM-2422 */}
+
             <MyConsumptionChart
                 isMetricsLoading={isMetricsLoading}
                 data={data}
@@ -90,6 +104,6 @@ export const MyConsumptionContainer = () => {
 
             {/* TODO: MYEM-2425 */}
             <MyConsumptionPeriod setPeriod={setPeriod} setRange={setRange} setPeriodValue={setPeriodValue} />
-        </>
+        </div>
     )
 }
