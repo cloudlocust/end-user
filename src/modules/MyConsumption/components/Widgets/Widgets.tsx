3cee9a7cf660943ab51da2e95f7b265fd19e1bfe
Amine Belmahi <amine@myem.fr>
2022-06-27 18:04:48 +0200

    MYEM-2623 #time 3h #comment Refactor to use loadash functions

--- src/modules/MyConsumption/components/Widgets/Widgets.tsx
+++ src/modules/MyConsumption/components/Widgets/Widgets.tsx
@@ -3,12 +3,8 @@ import { IWidgetAssets, IWidgetProps } from 'src/modules/MyConsumption/component
 import { useTheme } from '@mui/material'
 import { convertMetricsDataToApexChartsAxisValues } from 'src/modules/MyConsumption/utils/apexChartsDataConverter'
 import { IMetric } from 'src/modules/Metrics/Metrics'
-import {
-    calculateAverage,
-    calculateMaxNumber,
-    calculateSum,
-} from 'src/modules/MyConsumption/utils/MyConsumptionFunctions'
 import Grid from '@mui/material/Grid'
+import _ from 'lodash'
 
 /**
  * Function to get the values from data.
@@ -38,13 +34,47 @@ export const Widget = ({ type, data, isMetricsLoading }: IWidgetProps) => {
     const theme = useTheme()
 
     /**
+     * Render widgetr function.
+     *
+     * @param title Title of widget.
+     * @param unit Unit of widget.
+     * @param value Value of widget.
+     * @returns Widget component.
+     */
+    const renderWidget = (title: string, unit: string, value: number) => {
+        return (
+            <div className="p-16 flex flex-col justify-between">
+                <Typography className="sm:text-17 md:text-18 font-normal" style={{ minHeight: '65px' }}>
+                    {title}
+                </Typography>
+                <div className="flex flex-row flex-wrap mt-12 items-end">
+                    {/* Widget value */}
+                    <Typography className="text-3xl sm:text-4xl md:text-5xl font-normal tracking-tighter items-end mr-auto">
+                        {value}
+                    </Typography>
+                    <div className="flex flex-col">
+                        {/* Widget unit */}
+                        <Typography className="text-18 font-medium mb-24" color="textSecondary">
+                            {unit}
+                        </Typography>
+                        {/* TODDO MYEM-2588*/}
+                        {/* Widget arrow */}
+                        {/* <Typography className="font-medium text-base" color="textSecondary">
+                    Arrow
+                </Typography> */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    /**
      * Function that return value according to the type of widget.
      *
      * @param type Type from props.
-     * @param data Data from props.
      * @returns Data according to widget type.
      */
-    const getValueAccordingToWidgetType = (type: IWidgetProps['type'], data: IWidgetProps['data']) => {
+    const getValueAccordingToWidgetType = (type: IWidgetProps['type']) => {
         let totalConsumption: number = 0
         let maxPower: number = 0
         let averageInteranalTemp: number = 0
@@ -52,33 +82,26 @@ export const Widget = ({ type, data, isMetricsLoading }: IWidgetProps) => {
 
         if (type === 'consumption_metrics' && data.find((el) => el.target === type)) {
             const values = getValuesFromData(data)
-            totalConsumption = calculateSum(values)
+            totalConsumption = _.sum(values)
+            return totalConsumption
         }
 
         if (type === 'enedis_max_power' && data.find((el) => el.target === type)) {
             const values = getValuesFromData(data)
-            maxPower = calculateMaxNumber(values)
+            maxPower = _.max(values) as number
+            return maxPower
         }
 
         if (type === 'external_temperature_metrics' && data.find((el) => el.target === type)) {
             const values = getValuesFromData(data)
-            averageExternalTemp = calculateAverage(values)
+            averageExternalTemp = _.mean(values)
+            return averageExternalTemp
         }
 
         if (type === 'nrlink_internal_temperature_metrics' && data.find((el) => el.target === type)) {
             const values = getValuesFromData(data)
-            averageInteranalTemp = calculateAverage(values)
-        }
-
-        switch (type) {
-            case 'consumption_metrics':
-                return totalConsumption
-            case 'enedis_max_power':
-                return maxPower
-            case 'external_temperature_metrics':
-                return averageExternalTemp
-            case 'nrlink_internal_temperature_metrics':
-                return averageInteranalTemp
+            averageInteranalTemp = _.mean(values)
+            return averageInteranalTemp
         }
     }
 
@@ -86,47 +109,34 @@ export const Widget = ({ type, data, isMetricsLoading }: IWidgetProps) => {
      * Function that returns widget title, unit, and value according to type.
      *
      * @param type Widget type.
-     * @param element Widget elements like title, unit.
-     * @returns Title according to the widget type.
+     * @returns Widget with its assets.
      */
-    const renderWidgetAssets = (type: IWidgetProps['type'], element: 'title' | 'unit' | 'value') => {
-        let widgetTitle!: string
-        let widgetUnit!: string
-        let widgetValue!: number
+    const renderWidgetAssets = (type: IWidgetProps['type']) => {
+        let widgetTitle: string
+        let widgetUnit: string
+        let widgetValue: number
         // Render widget information according to the type from props.
         switch (type) {
             case 'consumption_metrics':
                 widgetTitle = 'Consommation totale' as IWidgetAssets['title']
                 widgetUnit = 'kWh' as IWidgetAssets['unit']
-                widgetValue = getValueAccordingToWidgetType('consumption_metrics', data) as number
-                break
+                widgetValue = getValueAccordingToWidgetType('consumption_metrics') as number
+                return renderWidget(widgetTitle, widgetUnit, widgetValue)
             case 'enedis_max_power':
                 widgetTitle = 'Puissance max' as IWidgetAssets['title']
                 widgetUnit = 'kVa' as IWidgetAssets['unit']
-                widgetValue = getValueAccordingToWidgetType('enedis_max_power', data) as number
-                break
+                widgetValue = getValueAccordingToWidgetType('enedis_max_power') as number
+                return renderWidget(widgetTitle, widgetUnit, widgetValue)
             case 'nrlink_internal_temperature_metrics':
                 widgetTitle = 'Température intérieure' as IWidgetAssets['title']
                 widgetUnit = '°C' as IWidgetAssets['unit']
-                widgetValue = getValueAccordingToWidgetType('nrlink_internal_temperature_metrics', data) as number
-                break
+                widgetValue = getValueAccordingToWidgetType('nrlink_internal_temperature_metrics') as number
+                return renderWidget(widgetTitle, widgetUnit, widgetValue)
             case 'external_temperature_metrics':
                 widgetTitle = 'Température extérieure' as IWidgetAssets['title']
                 widgetUnit = '°C' as IWidgetAssets['unit']
-                widgetValue = getValueAccordingToWidgetType('external_temperature_metrics', data) as number
-                break
-            default:
-                break
-        }
-
-        // Render asset according to the element.
-        switch (element) {
-            case 'title':
-                return widgetTitle
-            case 'unit':
-                return widgetUnit
-            case 'value':
-                return widgetValue
+                widgetValue = getValueAccordingToWidgetType('external_temperature_metrics') as number
+                return renderWidget(widgetTitle, widgetUnit, widgetValue)
             default:
                 break
         }
@@ -146,28 +156,7 @@ export const Widget = ({ type, data, isMetricsLoading }: IWidgetProps) => {
                         <CircularProgress style={{ color: theme.palette.primary.main }} />
                     </div>
                 ) : (
-                    <div className="p-16 flex flex-col justify-between">
-                        <Typography className="sm:text-17 md:text-18 font-normal" style={{ minHeight: '65px' }}>
-                            {renderWidgetAssets(type, 'title')}
-                        </Typography>
-                        <div className="flex flex-row flex-wrap mt-12 items-end">
-                            {/* Widget value */}
-                            <Typography className="text-3xl sm:text-4xl md:text-5xl font-normal tracking-tighter items-end mr-auto">
-                                {renderWidgetAssets(type, 'value')}
-                            </Typography>
-                            <div className="flex flex-col">
-                                {/* Widget unit */}
-                                <Typography className="text-18 font-medium mb-24" color="textSecondary">
-                                    {renderWidgetAssets(type, 'unit')}
-                                </Typography>
-                                {/* TODDO MYEM-2588*/}
-                                {/* Widget arrow */}
-                                {/* <Typography className="font-medium text-base" color="textSecondary">
-                                    Arrow
-                                </Typography> */}
-                            </div>
-                        </div>
-                    </div>
+                    renderWidgetAssets(type)
                 )}
             </Card>
         </Grid>
