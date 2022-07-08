551cb21e304878613b943f0e64aea675b76dad98
Amine Belmahi <amine@myem.fr>
2022-07-08 11:20:03 +0200

    MYEM-2699 #time 1h #comment Added condition on metersList for showng error message

--- src/modules/MyConsumption/MyConsumptionContainer.tsx
+++ src/modules/MyConsumption/MyConsumptionContainer.tsx
@@ -65,7 +65,7 @@ export const MyConsumptionContainer = () => {
         if (filters.length > 0) {
             getConsents(filters[0].value)
         }
-    }, [filters, getConsents])
+    }, [filters, getConsents, metersList])
 
     /**
      * Show text according to interval.
@@ -101,7 +101,12 @@ export const MyConsumptionContainer = () => {
         }
     }
 
-    if (nrlinkConsent?.nrlinkConsentState === 'NONEXISTENT' && enedisConsent?.enedisConsentState === 'NONEXISTENT') {
+    // By checking if the metersList is true we make sure that if someone has skipped the step of connecting their PDL, they will see this error message.
+    // Else if they have a PDL, we check its consent.
+    if (
+        (nrlinkConsent?.nrlinkConsentState === 'NONEXISTENT' && enedisConsent?.enedisConsentState === 'NONEXISTENT') ||
+        (metersList && metersList.length === 0)
+    ) {
         return (
             <div className="container relative h-200 sm:h-256 p-16 sm:p-24 flex-col text-center flex items-center justify-center">
                 <>
