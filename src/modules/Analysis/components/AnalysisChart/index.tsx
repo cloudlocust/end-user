2df4da21a42b1233ac617711062e41847f54b318
MediMESS <mehdi@myem.fr>
2022-07-28 10:14:43 +0100

    MYEM-2677 #time 30m #comment Implement getAnalysisApexChartProps in AnalysisChart

--- src/modules/Analysis/components/AnalysisChart/index.tsx
+++ src/modules/Analysis/components/AnalysisChart/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react'
-import ReactDOM from 'react-dom'
 import ReactApexChart from 'react-apexcharts'
 import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
@@ -8,11 +7,10 @@ import { IMetric, metricTargetsEnum } from 'src/modules/Metrics/Metrics.d'
 import { getDataFromYAxis } from 'src/modules/MyConsumption/components/Widget/WidgetFunctions'
 import convert from 'convert-units'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
-import { getAnalysisApexChartOptions } from 'src/modules/Analysis/utils/analysisApexChartsOptions'
-import { normalizeValues } from 'src/modules/Analysis/utils/computationFunctions'
+import { getAnalysisApexChartProps } from 'src/modules/Analysis/utils/analysisApexChartsProps'
 
 const analysisChartClassname = 'apexcharts-inner apexcharts-graphical'
-const analysisChartTooltipClassname = 'apexcharts-tooltip'
+
 /**
  * Analysis Polar Area Chart.
  *
@@ -35,11 +33,9 @@ const AnalysisChart = ({
     const isMobile = useMediaQuery(theme.breakpoints.down('md'))
     const analysisChartContainerRef = useRef<HTMLDivElement>(null)
     const analysisChartCircleContentRef = useRef<HTMLDivElement>(null)
+
     // Getting consumption values from data.
-    let values: ApexNonAxisChartSeries =
-        data.length > 0
-            ? getDataFromYAxis(data, metricTargetsEnum.consumption).map((val) => convert(val).from('Wh').to('kWh'))
-            : []
+    const values: ApexNonAxisChartSeries = data.length ? getDataFromYAxis(data, metricTargetsEnum.consumption) : []
 
     useEffect(() => {
         // This UseEffect is used to do some styling, in order to dynamically style the CircleContent in the middle of the analysisChart.
@@ -67,57 +63,16 @@ const AnalysisChart = ({
         )
     }
 
-    const analysisApexChartOptions = getAnalysisApexChartOptions(values, theme)
-
-    analysisApexChartOptions.chart!.events = {
-        /**
-         * Showing tooltip when clicking selecting an element in the polarArea chart, this is mostly for mobile because tooltip doesn't show only if we click on element.
-         *
-         * @param e DOM Element clicked on, it can represent each element in the polarArea chart, or the chart container.
-         */
-        dataPointSelection(e) {
-            const tooltipContainerElement = document.getElementsByClassName(
-                analysisChartTooltipClassname,
-            )[0] as HTMLDivElement
-            const activeTooltipClassname = 'apexcharts-active'
-            const activeTooltipElement = tooltipContainerElement.firstElementChild as HTMLDivElement
-
-            // If we click an element on the chart, we show tooltip
-            tooltipContainerElement.style!.left = `${e.offsetX - 40}px`
-            tooltipContainerElement.style!.top = `${e.offsetY - 40}px`
-            tooltipContainerElement.classList.add(activeTooltipClassname)
-            tooltipContainerElement.style.display = 'flex'
-            const indexActiveElement = e.target.className.baseVal.slice(-1)
-            // If the element is already highlighted
-            if (e.target.instance.filterer) {
-                const textNodeActiveTooltipElement = React.createElement(
-                    'span',
-                    {},
-                    `${values[indexActiveElement].toFixed(2)} kWh`,
-                )
-                ReactDOM.render(
-                    textNodeActiveTooltipElement,
-                    activeTooltipElement.querySelector('.apexcharts-tooltip-text-y-value'),
-                )
-                activeTooltipElement.style.background = theme.palette.primary.main
-                activeTooltipElement.style.display = 'flex'
-                activeTooltipElement.classList.add(activeTooltipClassname)
-            } else {
-                tooltipContainerElement.classList.remove(activeTooltipClassname)
-                tooltipContainerElement.style.display = 'none'
-            }
-        },
-    }
-
-    // normalize values to [200, 150], to improve polarArea chart and show all values from min to max.
-    // Values taken of min: 150, max: 200, makes the bars big enough for the lowest value, while being able to show the CircleContent inside the chart.
-    const series = normalizeValues(values, 150, 200)
+    const analysisApexChartProps = getAnalysisApexChartProps(
+        // We convert Wh to kWh as analysisChart shows kWh tooltip values.
+        values.map((val) => convert(val).from('Wh').to('kWh')),
+        theme,
+    )
 
     return (
         <div className="analysisChartContainer px-24" ref={analysisChartContainerRef}>
             <ReactApexChart
-                options={analysisApexChartOptions}
-                series={series}
+                {...analysisApexChartProps}
                 type={'polarArea'}
                 height={isMobile ? 360 : 520}
                 width={'100%'}
