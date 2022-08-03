17c0c6ff257b24c426a5a066af10d31f7dd7029a
youcef <youcef@myem.fr>
2022-08-03 10:55:42 +0100

    MYEM-2794 #time 10m #comment took off margin of button

--- src/modules/MyHouse/components/HousingList/index.tsx
+++ src/modules/MyHouse/components/HousingList/index.tsx
@@ -102,7 +102,7 @@ const HousingList = () => {
                             </div>
                         )}
                     </div>
-                    <div className="flex justify-center mt-24 sm:mr-32">
+                    <div className="flex justify-center mt-24">
                         <Button
                             variant="outlined"
                             className="w-4/5 sm:w-auto"
