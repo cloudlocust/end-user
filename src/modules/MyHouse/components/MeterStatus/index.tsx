d93a8169b20ad1ffd31ab288baebf76e8bb69a3f
amen <amine@myem.fr>
2022-09-02 09:42:18 +0100

    MYEM-2951 #time 1h #comment Added unit test for MeterStatus with useConsent hook

--- src/modules/MyHouse/components/MeterStatus/index.tsx
+++ src/modules/MyHouse/components/MeterStatus/index.tsx
@@ -1,11 +1,14 @@
-import { Card, useTheme, Icon } from '@mui/material'
+import { Card, useTheme, Icon, CircularProgress } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
 import { ReactComponent as ContractIcon } from 'src/assets/images/content/housing/contract.svg'
 import { URL_MY_HOUSE } from 'src/modules/MyHouse/MyHouseConfig'
 import { MuiCardContent } from 'src/common/ui-kit'
 import { MeterStatusProps } from 'src/modules/MyHouse/components/MeterStatus/meterStatus.d'
-import { useIntl } from 'src/common/react-platform-translation'
+import { useConsents } from 'src/modules/Consents/consentsHook'
+import { useEffect } from 'react'
+import { nrlinkConsentStatus } from 'src/modules/Consents/Consents'
+import dayjs from 'dayjs'
 
 /**
  * Meter Status Component.
@@ -17,7 +20,79 @@ import { useIntl } from 'src/common/react-platform-translation'
  */
 export const MeterStatus = ({ houseId, meterGuid }: MeterStatusProps) => {
     const theme = useTheme()
-    const { formatMessage } = useIntl()
+    const { getConsents, consentsLoading, nrlinkConsent } = useConsents()
+
+    const nrlinkConsentCreatedAt = dayjs(nrlinkConsent?.createdAt).format('DD/MM/YYYY')
+
+    useEffect(() => {
+        getConsents(meterGuid)
+    }, [getConsents, meterGuid])
+
+    /**
+     * Function that renders JSX accorrding to nrlink status.
+     *
+     * @param nrlinkStatus Different nrlink statuses.
+     * @returns JSX according to nrlink Status.
+     */
+    function renderNrlinkStatus(nrlinkStatus?: nrlinkConsentStatus) {
+        switch (nrlinkStatus) {
+            case 'CONNECTED':
+                return (
+                    <>
+                        <Icon className="mr-12">
+                            <img
+                                src="/assets/images/content/housing/consent-status/meter-on.svg"
+                                alt="connected-icon"
+                            />
+                        </Icon>
+                        <div className="flex flex-col">
+                            <span className="text-grey-600">{`nrLink n° ${nrlinkConsent?.nrlinkGuid}`}</span>
+                            <span className="text-grey-600">{`Connexion le ${nrlinkConsentCreatedAt}`}</span>
+                        </div>
+                    </>
+                )
+            case 'DISCONNECTED':
+                return (
+                    <>
+                        <Icon className="mr-12">
+                            <img src="/assets/images/content/housing/consent-status/meter-error.svg" alt="error-icon" />
+                        </Icon>
+                        <div className="flex flex-col">
+                            <TypographyFormatMessage color={theme.palette.warning.main} className="underline">
+                                Veuillez vérifier le branchement de votre appareil et/ou la connexion wifi.
+                            </TypographyFormatMessage>
+                        </div>
+                    </>
+                )
+            case 'EXPIRED':
+            case 'NONEXISTENT':
+                return (
+                    <>
+                        <Icon className="mr-12">
+                            <img src="/assets/images/content/housing/consent-status/meter-off.svg" alt="off-icon" />
+                        </Icon>
+                        <div className="flex flex-col">
+                            <TypographyFormatMessage color={theme.palette.error.main} className="underline">
+                                Connectez votre nrLINK pour visualiser votre consommation.
+                            </TypographyFormatMessage>
+                        </div>
+                    </>
+                )
+            default:
+                return (
+                    <>
+                        <Icon className="mr-12">
+                            <img src="/assets/images/content/housing/consent-status/meter-off.svg" alt="off-icon" />
+                        </Icon>
+                        <div className="flex flex-col">
+                            <TypographyFormatMessage color={theme.palette.error.main} className="underline">
+                                C'est erreur est survenue.
+                            </TypographyFormatMessage>
+                        </div>
+                    </>
+                )
+        }
+    }
 
     return (
         <Card className="my-12 md:mx-16" variant="outlined">
@@ -46,27 +121,21 @@ export const MeterStatus = ({ houseId, meterGuid }: MeterStatusProps) => {
                 <div className="flex flex-col md:flex-row justify-around">
                     {/* Nrlink Consent Status */}
                     <div className="p-12 border-b-1 border-grey-300">
-                        <TypographyFormatMessage className="text-xs md:text-sm font-semibold">
-                            Consommation en temps réel
-                        </TypographyFormatMessage>
-                        <div className="flex flex-row items-center">
-                            <Icon className="mr-12">
-                                <img
-                                    src="/assets/images/content/housing/consent-status/meter-on.svg"
-                                    alt="meter-status"
-                                />
-                            </Icon>
-                            {/* TODO: MYEM-2951 To be remplaced by consent api response */}
-                            <div className="flex flex-col">
-                                <span className="text-grey-600">nrLink n° 0CA2F4OOFFO45O65</span>
-                                <span className="text-grey-600">
-                                    {formatMessage({ defaultMessage: 'Connexion le', id: 'Connexion le' })} 05/03/2020
-                                </span>
-                            </div>
-                        </div>
+                        {consentsLoading ? (
+                            <CircularProgress size={25} />
+                        ) : (
+                            <>
+                                <TypographyFormatMessage className="text-xs md:text-sm font-semibold">
+                                    Consommation en temps réel
+                                </TypographyFormatMessage>
+                                <div className="flex flex-row items-center">
+                                    {renderNrlinkStatus(nrlinkConsent?.nrlinkConsentState)}
+                                </div>
+                            </>
+                        )}
                     </div>
                     {/* Enedis Consent Status */}
-                    {/* TODO: Remove hidden class in MYEM-2555 */}
+                    {/* TODO: To be worked on in MYEM-2555 */}
                     <div className="p-12 border-b-1 border-grey-300 hidden">
                         <TypographyFormatMessage className="text-xs md:text-sm font-semibold">
                             Historique de consommation
@@ -78,7 +147,6 @@ export const MeterStatus = ({ houseId, meterGuid }: MeterStatusProps) => {
                                     alt="meter-status"
                                 />
                             </Icon>
-                            {/* TODO: MYEM-2951 To be remplaced by consent api response */}
                             <div className="flex flex-col">
                                 <span className="text-grey-600">Date de fin de consentement</span>
                                 <span className="text-grey-600">01/01/2030</span>
