d978ba7772ed7a0e60c14a4d9584da146f564daa
kseniabezugla <kseniia@myem.fr>
2022-09-09 12:53:58 +0200

    MYEM-3019  #time 15m   #comment Added comments to FAQField

--- src/modules/FAQ/components/FAQFileld/FAQField.tsx
+++ src/modules/FAQ/components/FAQFileld/FAQField.tsx
@@ -3,21 +3,24 @@ import { Icon } from 'src/common/ui-kit'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
 import { IFAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField.d'
 /**
- * FAQField.
+ * FAQField component.
  *
+ * @param root0 N/A.
+ * @param root0.title FAQ title.
+ * @param root0.content FAQ content.
  * @returns FAQField component.
  */
 export const FAQField = ({ title, content }: IFAQField) => {
     const [isExpanded, setIsExpanded] = useState(false)
     return (
-        <div className="border rounded m-16 md:ml-36 border-gray-400 p-16 max-w-screen-md">
+        <div className="border rounded m-16 md:ml-36 border-gray-400 p-16 max-w-screen-sm">
             <div className="flex flex-row justify-between">
                 <TypographyFormatMessage className="self-center" sx={{ color: 'text.primary' }}>
                     {title}
                 </TypographyFormatMessage>
                 <Icon
                     className={`${isExpanded && 'transform rotate-45 transition-transform'}`}
-                    onClick={() => setIsExpanded(!isExpanded)}
+                    onClick={() => setIsExpanded((prevState) => !prevState)}
                     sx={{ color: 'primary.dark' }}
                 >
                     add
