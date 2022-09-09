6f2bb7ab9bffa485f99f217fd7997676fd19d1a6
kseniabezugla <kseniia@myem.fr>
2022-09-09 12:46:31 +0200

    MYEM-3019  #time 15m   #comment Fixed naming

--- src/modules/FAQ/components/FAQFileld/FAQField.d.ts
+++ src/modules/FAQ/components/FAQFileld/FAQField.d.ts
@@ -3,11 +3,11 @@
  */
 export interface IFAQField {
     /**
-     *
+     * FAQ title.
      */
     title: string
     /**
-     *
+     * FAQ content.
      */
     content: string
 }
--- src/modules/FAQ/components/FAQFileld/FAQField.test.tsx
+++ src/modules/FAQ/components/FAQFileld/FAQField.test.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import userEvent from '@testing-library/user-event'
 import { reduxedRender } from 'src/common/react-platform-components/test'
-import { FAQField } from './FAQField'
+import { FAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField'
 
 const propsFAQField = {
     title: 'Titre de la question',
     content: 'Réponse à la question',
 }
 
-describe('<FAQField /> props', () => {
-    test('should use correctly by FAQField', async () => {
-        const {  getByText } = reduxedRender(
+describe('<FAQField />', () => {
+    test('displaying the title and content passed to the field', async () => {
+        const { getByText } = reduxedRender(
             // eslint-disable-next-line jsdoc/require-jsdoc
             <FAQField {...propsFAQField} />,
         )
-
         expect(getByText(propsFAQField.title)).toBeTruthy()
         expect(getByText('add')).toBeTruthy()
         userEvent.click(getByText('add'))
