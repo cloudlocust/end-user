bdce2a463d853de519785d149a566e466daf1bfd
kseniabezugla <kseniia@myem.fr>
2022-09-12 13:01:29 +0200

    MYEM-3019  #time 20m  #comment Fixed test

--- src/modules/FAQ/components/FAQFileld/FAQField.test.tsx
+++ src/modules/FAQ/components/FAQFileld/FAQField.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import userEvent from '@testing-library/user-event'
 import { reduxedRender } from 'src/common/react-platform-components/test'
 import { FAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField'
+import { waitFor } from '@testing-library/react'
 
 const propsFAQField = {
     title: 'Titre de la question',
@@ -15,8 +16,13 @@ describe('<FAQField />', () => {
             <FAQField {...propsFAQField} />,
         )
         expect(getByText(propsFAQField.title)).toBeTruthy()
-        expect(getByText('add')).toBeTruthy()
+        // Open content
         userEvent.click(getByText('add'))
         expect(getByText(propsFAQField.content)).toBeTruthy()
+        // Close Content
+        userEvent.click(getByText('add'))
+        await waitFor(() => {
+            expect(() => getByText(propsFAQField.content)).toThrow()
+        })
     })
 })
