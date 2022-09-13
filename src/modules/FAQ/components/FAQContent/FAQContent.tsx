9317aaa1b6bc685f8766012f4f8c648660592d9d
kseniabezugla <kseniia@myem.fr>
2022-09-13 15:52:41 +0200

    MYEM-3020  #time 1h  #comment Added button

--- src/modules/FAQ/components/FAQContent/FAQContent.tsx
+++ src/modules/FAQ/components/FAQContent/FAQContent.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useFAQ } from 'src/modules/FAQ/FAQHook/FAQhook'
 import { FAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField'
+import { Button } from '@mui/material'
+import { useIntl } from 'react-intl'
 /**
  * FAQContent component.
  *
@@ -8,16 +10,27 @@ import { FAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField'
  */
 export const FAQContent = () => {
     const { dataFAQ, loadFAQ } = useFAQ()
+    const { formatMessage } = useIntl()
+
     useEffect(() => {
         if (!dataFAQ) {
             loadFAQ()
         }
     }, [dataFAQ, loadFAQ])
+
     return (
         <>
             {dataFAQ?.map((item) => (
-                <FAQField content={item.content} title={item.title} />
+                <FAQField content={item.content} title={item.title} key={item.id} />
             ))}
+            <div className="m-16 md:ml-36">
+                <Button variant="contained">
+                    {formatMessage({
+                        id: 'Contacter le Support Technique',
+                        defaultMessage: 'Contacter le Support Technique',
+                    })}
+                </Button>
+            </div>
         </>
     )
 }
