0b2c15e1ae71f08f12fa5fdbae46e25e6551bb10
kseniabezugla <kseniia@myem.fr>
2022-09-13 18:05:56 +0200

    MYEM-3020  #time 1m  #comment Removed comment

--- src/modules/FAQ/components/FAQContent/FAQContent.tsx
+++ src/modules/FAQ/components/FAQContent/FAQContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useFAQ } from 'src/modules/FAQ/FAQHook/FAQhook'
 import { FAQField } from 'src/modules/FAQ/components/FAQFileld/FAQField'
 import { Button, CircularProgress } from '@mui/material'
