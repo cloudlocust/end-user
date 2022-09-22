0f4b496178a333a2af1f0d2572df7cac6d78b205
amen <amine@myem.fr>
2022-09-22 17:24:06 +0100

    MYEM-3105 #time 2h #comment Implemented UI of InstallationRequestHeader

---
+++ src/modules/InstallatinRequests/components/InstallationRequestsTable.tsx
@@ -0,0 +1,8 @@
+/**
+ * Installation request table.
+ *
+ * @returns Installation request table JSX.
+ */
+export const InstallationRequestsTable = (): JSX.Element => {
+    return <>Installation request table</>
+}
--- src/modules/InstallatinRequests/index.tsx
+++ src/modules/InstallatinRequests/index.tsx
@@ -1,8 +1,30 @@
+import { styled } from '@mui/material/styles'
+import FusePageCarded from 'src/common/ui-kit/fuse/components/FusePageCarded'
+import { InstallationRequestsHeader } from 'src/modules/InstallatinRequests/components/InstallationRequestsHeader'
+
+const Root = styled(FusePageCarded)(({ theme }) => ({
+    '& .FusePageCarded-header': {
+        minHeight: 72,
+        height: 72,
+        alignItems: 'center',
+        [theme.breakpoints.up('sm')]: {
+            minHeight: 136,
+            height: 136,
+        },
+    },
+    '& .FusePageCarded-content': {
+        display: 'flex',
+    },
+    '& .FusePageCarded-contentCard': {
+        overflow: 'hidden',
+    },
+}))
+
 /**
  * Installation Requests page.
  *
  * @returns Installation Request JSX.
  */
 export const InstallationRequests = (): JSX.Element => {
-    return <>Instalation Requests</>
+    return <Root header={<InstallationRequestsHeader />} content={<>Content</>} innerScroll />
 }
