5aff7802a3db3348eb3024b859114ccaa6d9cd58
amen <amine@myem.fr>
2022-09-22 10:28:13 +0100

    MYEM-3099 #time 1h #comment Create route for installation request w/ UserMenu button

---
+++ src/modules/InstallatinRequests/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * Installation Requests page.
+ *
+ * @returns Installation Request JSX.
+ */
+export const InstallationRequests = (): JSX.Element => {
+    return <>Instalation Requests</>
+}
