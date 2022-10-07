db28861c45b5e8e95e39237028f61431e5826bf5
amen <amine@myem.fr>
2022-10-07 09:24:14 +0100

    MYEM-3149 #time 1d #comment Optimize re-render for TargetButtonGroup & MyConsumptionPeriod & MyConsumptionChart

--- src/hooks/RenderCounter/index.ts
+++ src/hooks/RenderCounter/index.ts
@@ -1,19 +1,22 @@
 import { useRef } from 'react'
 
-const SHOW_RENDER_COUNTERS = true
-
 /**
- *  Hook that show renderCount.
+ * Hook that show renderCount.
  *
+ * In order to get the component name dynamically.
+ * Let's suppose the name is named Home, you pass Home.name as an argument to the hook.
+ * Then, the hook will console log the component name followed by the renderCount.
+ *
+ * @param componentName Component name to be passed as arg.
  * @returns Render count.
  */
-export const useRenderCounter = () => {
+export const useRenderCounter = (componentName: string) => {
     const renderCount = useRef(0)
     renderCount.current = renderCount.current + 1
 
-    if (process.env.NODE_ENV === 'development' && SHOW_RENDER_COUNTERS) {
+    if (process.env.NODE_ENV === 'development') {
         // eslint-disable-next-line no-console
-        return console.log('RENDER COUNT: ', renderCount.current)
+        return console.log(`${componentName}`, renderCount.current)
     }
     return null
 }
