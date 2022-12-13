05eea4fae43c365d707b473f571c4d3594851dd8
youcef <youcef@myem.fr>
2022-12-13 17:17:30 +0100

    MYEM-3512 #time 15m #comment fixe after review

--- src/modules/Layout/Toolbar/components/Alerts/ConsumptionAlert/index.tsx
+++ src/modules/Layout/Toolbar/components/Alerts/ConsumptionAlert/index.tsx
@@ -3,7 +3,8 @@ import { Card, Button } from '@mui/material'
 import { useIntl } from 'react-intl'
 import TextField from '@mui/material/TextField'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
-import { ConsumptionAlertIntervalsType } from 'src/modules/Layout/Toolbar/components/Alerts/ConsumptionAlert/consumptionAlert'
+import { ConsumptionAlertIntervalsType } from './consumptionAlert'
+import { ConsumptionAlertTitle } from './consumptionAlertsVariables'
 
 /**
  * Consumption Alert Component.
@@ -17,15 +18,6 @@ const ConsumptionAlert = ({
 }: //eslint-disable-next-line
 { interval: ConsumptionAlertIntervalsType
 }) => {
-    /**
-     * Constant to have titles for each interval for the consumptions.
-     */
-    const ConsumptionAlertTitle = {
-        day: "Seuil d'alerte journalier",
-        week: "Seuil d'alerte hebdomadaire",
-        month: "Seuil d'alerte mensuel",
-    }
-
     const [isEdit, setIsEdit] = useState(false)
 
     return (
@@ -36,8 +28,36 @@ const ConsumptionAlert = ({
                         {ConsumptionAlertTitle[interval]}
                     </TypographyFormatMessage>
                     <div className="flex justify-around content-center mb-16">
-                        <CustomTextFieldInput endSymbol="kWh" disabled={!isEdit} />
-                        <CustomTextFieldInput endSymbol="€" disabled={!isEdit} />
+                        <div className="flex justify-center content-center">
+                            <TextField
+                                variant="outlined"
+                                sx={{ maxWidth: '75px' }}
+                                disabled={!isEdit}
+                                type="number"
+                                defaultValue={0}
+                            />
+                            <div
+                                className="bg-gray-400 flex items-center justify-center float-left rounded"
+                                style={{ width: 30, height: 51.69, marginLeft: -5, zIndex: 0 }}
+                            >
+                                <span>kWh</span>
+                            </div>
+                        </div>
+                        <div className="flex justify-center content-center">
+                            <TextField
+                                variant="outlined"
+                                sx={{ maxWidth: '75px' }}
+                                disabled={!isEdit}
+                                type="number"
+                                defaultValue={0}
+                            />
+                            <div
+                                className="bg-gray-400 flex items-center justify-center float-left rounded"
+                                style={{ width: 30, height: 51.69, marginLeft: -5, zIndex: 0 }}
+                            >
+                                <span>€</span>
+                            </div>
+                        </div>
                     </div>
                     <ButtonsGroup
                         isEdit={isEdit}
@@ -46,47 +66,6 @@ const ConsumptionAlert = ({
                     />
                 </div>
             </Card>
-        </div>
-    )
-}
-
-/**
- * Component for input field € and kWh.
- *
- * @param props N/A.
- * @param props.endSymbol Is the input a consumption or price.
- * @param props.disabled Is the field disabled.
- * @returns JSX.
- */
-const CustomTextFieldInput = ({
-    endSymbol,
-    disabled,
-}: //eslint-disable-next-line
-{
-    /**
-     * Text Field Input Type.
-     */
-    endSymbol: String
-    /**
-     * If field is Disabled.
-     */
-    disabled: boolean
-}) => {
-    return (
-        <div className="flex justify-center content-center">
-            <TextField
-                variant="outlined"
-                sx={{ maxWidth: '75px' }}
-                disabled={disabled}
-                type="number"
-                defaultValue={0}
-            />
-            <div
-                className="bg-gray-400 flex items-center justify-center float-left rounded"
-                style={{ width: 30, height: 51.69, marginLeft: -5, zIndex: 0 }}
-            >
-                <span>{endSymbol}</span>
-            </div>
         </div>
     )
 }
