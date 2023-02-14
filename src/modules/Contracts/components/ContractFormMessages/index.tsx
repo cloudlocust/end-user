2951045ae17209bb2d97433cc381d85859c43354
MediMESS <mehdi@myem.fr>
2023-02-14 10:57:21 +0100

    MYEM-3965 #time 5m #comment Fix Production PR of ContractForm Unit Test and Add OtherPRovider-OtherOffer options

---
+++ src/modules/Contracts/components/ContractFormMessages/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { OtherProviderOfferOptionMessageProps } from 'src/modules/Contracts/contractsTypes.d'
+import { Typography } from '@mui/material'
+
+/**
+ * OtherProviderOfferOptionMessage Component.
+ *
+ * @param props N/A.
+ * @param props.isShowMessage Indicates if the message component should be shown.
+ * @returns Default Contract Warning message component.
+ */
+export const OtherProviderOfferOptionMessage = ({ isShowMessage }: OtherProviderOfferOptionMessageProps) => {
+    if (!isShowMessage) return null
+    return (
+        <div>
+            <Typography className="text-10 md:text-13">
+                Veuillez contacter
+                <span> </span>
+                <a href="mailto:support@myem.fr" className="text-12 md:text-13 underline text-blue-500">
+                    support@myem.fr
+                </a>
+                <span> </span>
+                en précisant le fournisseur et l’offre que vous souhaitez voir renseignés
+            </Typography>
+        </div>
+    )
+}
