919a982deee771652d8a8145e9684ae81bbbf411
Amine Belmahi <amine@myem.fr>
2023-03-21 17:01:30 +0100

    MYEM-4169 #time 20m #comment Add env var for other privoders & offers

--- src/modules/Contracts/components/ContractForm/index.tsx
+++ src/modules/Contracts/components/ContractForm/index.tsx
@@ -22,6 +22,7 @@ import OffpeakHoursField from 'src/modules/Contracts/components/OffpeakHoursFiel
 import { useParams } from 'react-router-dom'
 import { useMeterForHousing } from 'src/modules/Meters/metersHook'
 import { OtherProviderOfferOptionMessage } from 'src/modules/Contracts/components/ContractFormMessages'
+import { isActivateOtherOffersAndProviders } from 'src/modules/Contracts/ContractsConfig'
 
 const defaultContractFormValues: contractFormValuesType = {
     contractTypeId: 0,
@@ -193,7 +194,7 @@ const ContractFormFields = ({ isContractsLoading }: ContractFormFieldsProps) => 
                         isOptionsInProgress={isProvidersLoading}
                         loadOptions={loadProviderOptions}
                         optionList={orderBy(providerList, 'name', 'asc')}
-                        otherOptionLabel="Autre fournisseur"
+                        otherOptionLabel={isActivateOtherOffersAndProviders ? 'Autre fournisseur' : undefined}
                         name="providerId"
                         label="Fournisseur"
                         validateFunctions={[requiredBuilder()]}
@@ -209,7 +210,7 @@ const ContractFormFields = ({ isContractsLoading }: ContractFormFieldsProps) => 
                         formatOptionValue={(option) => option.id}
                         isOptionsInProgress={isOffersLoading}
                         loadOptions={loadOfferOptions}
-                        otherOptionLabel="Autre offre"
+                        otherOptionLabel={isActivateOtherOffersAndProviders ? 'Autre offre' : undefined}
                         optionList={orderBy(offerList, 'name', 'asc')}
                         name="offerId"
                         label="Offre"
