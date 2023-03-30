348e62a5572a8ab313d31dda3a7af90e18bfa85f
Amine Belmahi <amine@myem.fr>
2023-03-30 14:40:05 +0100

    MYEM-4198 #time 8h #comment Implement MultiTab component to analyse page

--- src/common/ui-kit/components/MultiTab/MultiTab.tsx
+++ src/common/ui-kit/components/MultiTab/MultiTab.tsx
@@ -4,8 +4,9 @@ import { styled } from '@mui/material/styles'
 import FusePageCarded from 'src/common/ui-kit/fuse/components/FusePageCarded'
 import { useIntl } from 'src/common/react-platform-translation'
 import Tabs from '@mui/material/Tabs'
-import Tab from '@mui/material/Tab'
+import Tab, { TabTypeMap } from '@mui/material/Tab'
 import { keyBy, mapValues } from 'lodash'
+import { TabsTypeMap } from '@mui/material/Tabs'
 
 const Root = styled(FusePageCarded)(({ theme }) => ({
     '& .FusePageCarded-header': {
@@ -25,14 +26,28 @@ const Root = styled(FusePageCarded)(({ theme }) => ({
     },
 }))
 
-//eslint-disable-next-line jsdoc/require-jsdoc
+/**
+ * MuiTabProps.
+ */
 export interface IMultiTab {
-    //eslint-disable-next-line jsdoc/require-jsdoc
+    /**
+     * Tab title.
+     */
     tabTitle: string
-    //eslint-disable-next-line jsdoc/require-jsdoc
+    /**
+     * Tab slug.
+     *
+     * @example "/this-is-a-slug"
+     */
     tabSlug: string
-    //eslint-disable-next-line jsdoc/require-jsdoc
+    /**
+     * Tab content.
+     */
     tabContent: JSX.Element
+    /**
+     * Tab icon.
+     */
+    icon?: string | React.ReactElement<any, string | React.JSXElementConstructor<any>>
 }
 
 /**
@@ -43,29 +58,40 @@ export interface IMultiTab {
  * @param props.header The Header Component of the Tab.
  * @param props.content Content that will be displayed (format : IMultiTab).
  * @param props.innerScroll Indicates if there is an innerScroll inside the tabs.
+ * @param props.TabsProps Rest of Tabs props.
+ * @param props.TabProps Rest of Tab props.
  * @returns  Element Details Tabs.
  */
 const MultiTab = ({
-    /**
-     * The header above the tabs.
-     */
     header,
-    /**
-     *  All components supposed to be in the tabs (with there title, slug, content).
-     */
     content,
-    // eslint-disable-next-line jsdoc/require-jsdoc
     innerScroll,
+    TabsProps,
+    TabProps,
 }: /**
  *
  */
 {
-    //eslint-disable-next-line jsdoc/require-jsdoc
+    /**
+     * The header above the tabs.
+     */
     header?: JSX.Element
-    //eslint-disable-next-line jsdoc/require-jsdoc
-    content: Array<IMultiTab>
-    //eslint-disable-next-line jsdoc/require-jsdoc
+    /**
+     *  All components supposed to be in the tabs (with there title, slug, content).
+     */
+    content: IMultiTab[]
+    /**
+     * Boolean for innerScroll.
+     */
     innerScroll?: boolean
+    /**
+     * Rest of tabs props.
+     */
+    TabsProps?: TabsTypeMap['props']
+    /**
+     * Rest of tab props.
+     */
+    TabProps?: TabTypeMap['props']
 }) => {
     const { formatMessage } = useIntl()
 
@@ -79,6 +105,19 @@ const MultiTab = ({
     // Initialise Base path and entry Tab.
     let entryTab = location.pop()
     let basePath = location.join('/')
+
+    /**
+     * @description With this modification, if the location array is empty after the split() method,
+     * the entryTab variable will be set to the tabSlug of the first tab in the content array,
+     * and the basePath variable will be set to the original pathname.
+     * This will allow the component to handle the /route/child-route URL as well
+     * instead of handling only /route/first-child/second-child.
+     */
+    if (!entryTab) {
+        entryTab = content[0].tabSlug
+        basePath = pathname
+    }
+
     // UseHistory, and tab Handle
     const tabSlugList = content.filter((item) => item.tabSlug === entryTab)
     const isInvalidValue = !tabSlugList.length || !entryTab?.length
@@ -108,6 +147,7 @@ const MultiTab = ({
                     variant="scrollable"
                     scrollButtons="auto"
                     classes={{ root: 'w-full h-64' }}
+                    {...TabsProps}
                 >
                     {content.map((element, index) => (
                         <Tab
@@ -115,6 +155,8 @@ const MultiTab = ({
                             value={element.tabSlug}
                             label={formatMessage({ id: element.tabTitle, defaultMessage: element.tabTitle })}
                             className="h-64"
+                            icon={element.icon}
+                            {...TabProps}
                         />
                     ))}
                 </Tabs>
