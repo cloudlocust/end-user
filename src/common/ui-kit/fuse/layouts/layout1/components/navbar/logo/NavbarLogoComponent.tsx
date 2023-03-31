cc4b0b24f75c4a20cb73a5d051e3b68b6526f783
Alexis Montelimart <alexis@myem.fr>
2023-03-31 10:25:49 +0200

    MYEM-4201 #time 10m #comment fix: responsive

--- src/common/ui-kit/fuse/layouts/layout1/components/navbar/logo/NavbarLogoComponent.tsx
+++ src/common/ui-kit/fuse/layouts/layout1/components/navbar/logo/NavbarLogoComponent.tsx
@@ -10,11 +10,11 @@ import { useMediaQuery, useTheme } from '@mui/material'
  */
 const NavbarLogoComponent = () => {
     const theme = useTheme()
-    const mdUp = useMediaQuery(theme.breakpoints.up('md'))
+    const lgUp = useMediaQuery(theme.breakpoints.up('lg'))
 
     const clientApplicationURL: string | undefined = CLIENT_WEBSITE_URL
 
-    const logoPath = mdUp
+    const logoPath = lgUp
         ? `./clients-icons/${CLIENT_ICON_FOLDER}/${CLIENT_ICON_FOLDER}-on-primary.svg`
         : `./clients-icons/${CLIENT_ICON_FOLDER}/${CLIENT_ICON_FOLDER}.svg`
 
@@ -23,7 +23,7 @@ const NavbarLogoComponent = () => {
             className={clientApplicationURL ? 'hoverableLogoLink' : ''}
             href={clientApplicationURL ? clientApplicationURL : undefined}
         >
-            <img className={mdUp ? 'my-32' : 'ml-8'} src={logoPath} width={mdUp ? '60' : '45'} alt="logo" />
+            <img className={lgUp ? 'my-32' : 'ml-8'} src={logoPath} width={lgUp ? '60' : '45'} alt="logo" />
         </a>
     )
 }
