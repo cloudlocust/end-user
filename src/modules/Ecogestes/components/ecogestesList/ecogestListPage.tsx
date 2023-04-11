f6288cdb595287dfad2efd36de545388ac91ae69
7eith <alexis@myem.fr>
2023-04-11 10:52:23 +0200

    MYEM-4115 #time 30m #comment ref: splitted Page (header part)

--- src/modules/Ecogestes/components/ecogestesList/ecogestListPage.tsx
+++ src/modules/Ecogestes/components/ecogestesList/ecogestListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import PageSimple from 'src/common/ui-kit/fuse/components/PageSimple'
 import { ThemeProvider, useTheme } from '@mui/material/styles'
 import EcogestesList from 'src/modules/Ecogestes/components/ecogestesList'
@@ -18,11 +18,11 @@ const PLACEHOLDER_ALL_TAGS_HEADER: IEcogestTag = {
 }
 
 /**
- * Props to pass to <EcogesteListContent.
+ * Default Props for the EcogestPage Components.
  */
-type IEcogestListContentProps =
+type IEcogestPageComponentProps =
     /**
-     * Props to pass to <EcogesteListContent.
+     * Default Props for the EcogestPage Components.
      */
     {
         /**
@@ -32,18 +32,83 @@ type IEcogestListContentProps =
         /**
          * Current List of Ecogeste Tags.
          */
-        elementList: IEcogestTag[] | null
+        elementList?: IEcogestTag[] | null | undefined
     }
 
 /**
- * EcogestListContent Component.
+ * Props for Ecogest Header Component.
+ */
+interface IEcogestHeaderProps extends IEcogestPageComponentProps {
+    /**
+     * Loading state of @useEcogestTags Hooks.
+     */
+    isLoading: boolean
+}
+
+/**
+ * EcogestList Header Component.
+ *
+ * @param root0 Props (extend default component Props).
+ * @param root0.isLoading Loading State of @useEcogestTags Hooks.
+ * @param root0.currentTag Current Ecogest Tag.
+ * @returns HeaderComponent.
+ */
+const EcogestListHeader = ({ isLoading, currentTag }: IEcogestHeaderProps): JSX.Element => {
+    const theme = useTheme()
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div
+                className="w-full h-full p-10 flex flex-nowrap flex-col gap-1 justify-around"
+                style={{ backgroundColor: theme.palette.primary.dark }}
+            >
+                {!isLoading && currentTag && currentTag.name && currentTag.icon ? (
+                    <>
+                        <Icon
+                            sx={{ color: 'primary.contrastText' }}
+                            aria-hidden="true"
+                            color="inherit"
+                            style={{ height: '75%', alignSelf: 'center', width: '100%', margin: 'auto' }}
+                        >
+                            <img
+                                // A note about the filter shenanigans under here:
+                                // It works.
+                                // If you have a better idea, that still allows for dynamic icons to be given, please, do make a PR for it.
+                                // Until then, it works with black images :v
+                                style={{
+                                    filter: `opacity(0.1) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText})`,
+                                    height: '100%',
+                                    margin: 'auto',
+                                }}
+                                src={currentTag!.icon}
+                                alt=""
+                            ></img>
+                        </Icon>
+
+                        <TypographyFormatMessage className="text-center text-20 font-semibold">
+                            {currentTag!.name}
+                        </TypographyFormatMessage>
+                    </>
+                ) : (
+                    <>
+                        <Skeleton variant="rectangular" width={'30%'} height={'75%'} sx={{ margin: 'auto' }} />
+                        <Skeleton variant="text" sx={{ fontSize: '2rem', margin: 'auto' }} width={'40%'} />
+                    </>
+                )}
+            </div>
+        </ThemeProvider>
+    )
+}
+
+/**
+ * EcogestList Content Component.
  *
  * @param root0 Props.
  * @param root0.currentTag Current Ecogest Tag.
  * @param root0.elementList Current List of Ecogeste Tags.
  * @returns JSX.Element.
  */
-const EcogestListContent = ({ currentTag, elementList }: IEcogestListContentProps): JSX.Element => {
+const EcogestListContent = ({ currentTag, elementList }: IEcogestPageComponentProps): JSX.Element => {
     return currentTag?.id && currentTag.id > 0 && elementList ? (
         <div className="m-10 p-10">
             <EcogestesList />
@@ -63,8 +128,6 @@ const EcogestListContent = ({ currentTag, elementList }: IEcogestListContentProp
  * @returns A page with a list of ecogestes.
  */
 const EcogestListPage = () => {
-    const theme = useTheme()
-
     const { tagId } = useParams</**
      * Params object.
      */
@@ -79,58 +142,17 @@ const EcogestListPage = () => {
 
     const { loadingInProgress, elementList } = useEcogesteTags()
 
-    const [curr_tag, setCurrTag] = useState<null | undefined | IEcogestTag>(PLACEHOLDER_ALL_TAGS_HEADER)
+    const [currentTag, setCurrentTag] = useState<null | undefined | IEcogestTag>(PLACEHOLDER_ALL_TAGS_HEADER)
 
     useEffect(() => {
         let tag = elementList?.find((element) => element.id === tagIdInt)
-        setCurrTag(tag ? tag : PLACEHOLDER_ALL_TAGS_HEADER)
+        setCurrentTag(tag ? tag : PLACEHOLDER_ALL_TAGS_HEADER)
     }, [elementList, tagIdInt])
 
     return (
         <PageSimple
-            header={
-                <ThemeProvider theme={theme}>
-                    <div
-                        className="w-full h-full p-10 flex flex-nowrap flex-col gap-1 justify-around"
-                        style={{ backgroundColor: theme.palette.primary.dark }}
-                    >
-                        {!loadingInProgress && curr_tag && curr_tag.name && curr_tag.icon ? (
-                            <>
-                                <Icon
-                                    sx={{ color: 'primary.contrastText' }}
-                                    aria-hidden="true"
-                                    color="inherit"
-                                    style={{ height: '75%', alignSelf: 'center', width: '100%', margin: 'auto' }}
-                                >
-                                    <img
-                                        // A note about the filter shenanigans under here:
-                                        // It works.
-                                        // If you have a better idea, that still allows for dynamic icons to be given, please, do make a PR for it.
-                                        // Until then, it works with black images :v
-                                        style={{
-                                            filter: `opacity(0.1) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText}) drop-shadow(0 0 0 ${theme.palette.primary.contrastText})`,
-                                            height: '100%',
-                                            margin: 'auto',
-                                        }}
-                                        src={curr_tag!.icon}
-                                        alt=""
-                                    ></img>
-                                </Icon>
-
-                                <TypographyFormatMessage className="text-center text-20 font-semibold">
-                                    {curr_tag!.name}
-                                </TypographyFormatMessage>
-                            </>
-                        ) : (
-                            <>
-                                <Skeleton variant="rectangular" width={'30%'} height={'75%'} sx={{ margin: 'auto' }} />
-                                <Skeleton variant="text" sx={{ fontSize: '2rem', margin: 'auto' }} width={'40%'} />
-                            </>
-                        )}
-                    </div>
-                </ThemeProvider>
-            }
-            content={<EcogestListContent currentTag={curr_tag} elementList={elementList} />}
+            header={<EcogestListHeader isLoading={loadingInProgress} currentTag={currentTag} />}
+            content={<EcogestListContent currentTag={currentTag} elementList={elementList} />}
         />
     )
 }
