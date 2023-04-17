8b64afb8bfb16fa343b65721f75572478f7edd70
Amine Belmahi <amine@myem.fr>
2023-04-17 09:34:50 +0100

    MYEM-4180 #time 1h #comment remove unnecessary complexity in computePmaxWithoutTimestamp

--- src/modules/Analysis/components/AnalysisMaxPower/utils.ts
+++ src/modules/Analysis/components/AnalysisMaxPower/utils.ts
@@ -10,21 +10,11 @@ import { IMetric, metricTargetsEnum } from 'src/modules/Metrics/Metrics.d'
 export const computePMaxWithTimestamp = (data: IMetric[]) => {
     let pmaxValue = 0
     let pmaxValueTimestamp = 0
-    let datapoints: number[][] = []
 
-    const pmaxMetric = data.filter((elem) => elem.target === metricTargetsEnum.pMax)
+    const pmaxMetric = data.find((elem) => elem.target === metricTargetsEnum.pMax)
 
-    if (pmaxMetric[0]) {
-        const entries = Object.entries(pmaxMetric[0])
-        for (const [key, objValue] of entries) {
-            if (key === 'datapoints' && typeof objValue === 'object') {
-                datapoints = objValue
-            }
-        }
-    }
-
-    datapoints.forEach(([value, timestamp]) => {
-        if (value > pmaxValue) {
+    pmaxMetric?.datapoints.forEach(([value, timestamp]) => {
+        if (Number(value) > pmaxValue) {
             pmaxValue = value
             pmaxValueTimestamp = timestamp
         }
