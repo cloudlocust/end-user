b06378e606a177e898fcf968be26b93f214a9bcb
Amine Belmahi <amine@myem.fr>
2023-04-25 08:03:33 +0100

    MYEM-4256 #time 1h #comment update procces-env.sh

--- ci/process-env.sh
+++ ci/process-env.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Recreate config file
-rm -rf /usr/share/nginx/html/env-config.js
-touch /usr/share/nginx/html/env-config.js
-
-# Add assignment 
-echo "window._env_ = {" >> /usr/share/nginx/html/env-config.js
-
 # Read each line in .env file
 # Each line represents key=value pairs
 while read -r line || [[ -n "$line" ]];
@@ -21,9 +14,15 @@ do
   value=$(printf '%s\n' "${!varname}")
   # Otherwise use value from .env file
   [[ -z $value ]] && value=${varvalue}
+
+  # Replace %REACT_APP_BASENAME_URL% variable in index.html file with value from .env.template
+  if [ "$varname" = "REACT_APP_BASENAME_URL" ]; then
+    sed -i "s/%REACT_APP_BASENAME_URL%/$value/g" /usr/share/nginx/html/index.html
+  fi
   
   # Append configuration property to JS file
   echo "  $varname: \"$value\"," >> /usr/share/nginx/html/env-config.js
 done < /usr/share/nginx/html/.env.template
 
+echo "window._env_ = {" >> /usr/share/nginx/html/env-config.js
 echo "}" >> /usr/share/nginx/html/env-config.js
