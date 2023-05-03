c2ff9c3b6be35a38d898f8f4221f888eeb9d7ec9
7eith <seith@synezia.com>
2023-05-03 17:07:10 +0200

    MYEM-4382 #time 1h30 #comment add: if one item, item is default

--- src/modules/Contracts/components/ContractFormSelect/index.tsx
+++ src/modules/Contracts/components/ContractFormSelect/index.tsx
@@ -39,12 +39,56 @@ const ContractFormSelect = <T extends unknown>({
         loadOptions()
     }, [loadOptions])
 
+    /**
+     * Eyes tears blood...
+     * I haven't found any work around that work better than that without a big refactor,
+     * the Form is managed with a logic, when a MenuItem::onChange is fired, its calling all parents onChange untill he call
+     * the onChange inside ContractForm, the thing is I can't touch to this, or the hooks cause its will make a big refactor,
+     * this is the best workAround for it.
+     * When he has no other option (list of option contain one choice, no otherOptionLabel so no custom fields),
+     * I call the onChange myself in the useEffect, the onChange just use target {...} not the ReactNode,
+     * I just listen on optionList, this @var will only have two change
+     * render -> where is Empty and dont call useEffect
+     * loaded -> when loadOptions have been a success so it will hidrate optionList once.
+     */
+
+    useEffect(() => {
+        if (!otherOptionLabel && optionList && optionList.length === 1 && otherSelectProps.onChange) {
+            otherSelectProps.onChange(
+                {
+                    target: {
+                        name: name,
+                        value: formatOptionValue(optionList[0]),
+                    },
+                } as any,
+                null,
+            )
+        } // eslint-disable-next-line
+    }, [optionList])
+
     if (isOptionsInProgress || isNull(optionList))
         return (
             <div style={{ display: 'flex', justifyContent: 'center', padding: '16px' }}>
                 <CircularProgress size={32} />
             </div>
         )
+
+    if (!otherOptionLabel && optionList && optionList.length === 1) {
+        return (
+            <Select
+                name={name}
+                label={formatMessage({
+                    id: `${label}`,
+                    defaultMessage: `${label}`,
+                })}
+                validateFunctions={validateFunctions}
+                disabled={true}
+                {...otherSelectProps}
+            >
+                <MenuItem value={formatOptionValue(optionList[0])}>{formatOptionLabel(optionList[0])}</MenuItem>
+            </Select>
+        )
+    }
 
     return (
         <>
