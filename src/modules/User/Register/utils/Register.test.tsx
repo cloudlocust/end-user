dfdb5c477cb6588b56c31835e9665579cbc0fb60
Amine Belmahi <amine@myem.fr>
2023-06-06 10:07:19 +0100

    MYEM-4573 #time 45m  #comment Add tests for Register.tsx & RenderCounter hook

---
+++ src/modules/User/Register/utils/Register.test.tsx
@@ -0,0 +1,45 @@
+import { waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter, MemoryRouter, Route } from 'react-router-dom'
+import { reduxedRender } from 'src/common/react-platform-components/test'
+import Register from 'src/modules/User/Register/Register'
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+const RegisterForm = () => {
+    return <div>Register form</div>
+}
+
+const REGISTER_TEXT = 'Inscription'
+const HAVE_ALREADY_ACCOUNT_TEXT = 'Vous avez déjà un compte ?'
+const LOGIN_TEXT = 'Se connecter'
+
+describe('Register component', () => {
+    test('register component text is rendered', async () => {
+        const { getByText } = reduxedRender(
+            <BrowserRouter>
+                <Register registerForm={<RegisterForm />} />
+            </BrowserRouter>,
+        )
+
+        expect(getByText(REGISTER_TEXT)).toBeInTheDocument()
+        expect(getByText('Register form')).toBeInTheDocument()
+        expect(getByText(HAVE_ALREADY_ACCOUNT_TEXT)).toBeInTheDocument()
+        expect(getByText(LOGIN_TEXT)).toBeInTheDocument()
+    })
+    test("when clicked on don't have account", async () => {
+        const { getByText } = reduxedRender(
+            <MemoryRouter initialEntries={['/register']}>
+                <BrowserRouter>
+                    <Register registerForm={<RegisterForm />} />
+                    <Route path="/login" component={() => <div>login</div>} />
+                </BrowserRouter>
+            </MemoryRouter>,
+        )
+
+        userEvent.click(getByText(LOGIN_TEXT))
+
+        await waitFor(() => {
+            expect(getByText('login')).toBeInTheDocument()
+        })
+    })
+})
