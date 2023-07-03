5452892dabe68b1dc97ab37e7e9ac3f696b27567
Zakaria laidi <zakaria@myem.fr>
2023-07-04 00:07:06 +0100

    MYEM-4715 #time 30m #comment enable addhousing form when there is no housing

--- src/modules/Layout/Toolbar/components/SelectHousing/index.tsx
+++ src/modules/Layout/Toolbar/components/SelectHousing/index.tsx
@@ -82,7 +82,6 @@ export const SelectHousing = () => {
                     input={<OutlinedInput label="Logement" />}
                     renderValue={() => <>{currentHousing?.address.name}</>}
                     MenuProps={MenuProps}
-                    disabled={!currentHousing?.id}
                 >
                     {housingList?.map((housing) => (
                         <MenuItem key={housing.id} value={housing.id} className="flex justify-between">
