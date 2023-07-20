15b608ce10780f7f0294fe4d752386a01a3b622c
Amine Belmahi <amine@myem.fr>
2023-07-20 16:29:40 +0100

    MYEM-4821 #time 1h #comment feat: invert tooltip order to match reverse of targets

--- src/modules/MyConsumption/utils/apexChartsMyConsumptionOptions.ts
+++ src/modules/MyConsumption/utils/apexChartsMyConsumptionOptions.ts
@@ -334,6 +334,7 @@ export const getApexChartMyConsumptionProps = ({
                 }
             },
         },
+        inverseOrder: isStackedEnabled ? true : false,
     }
 
     if (xAxisType === 'category') {
