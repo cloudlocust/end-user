44715547676f8f1fcde338102c2fc5aaad848a03
Abdelkadir <ga_bouziane@esi.dz>
2023-09-18 10:40:54 +0100

    MYEM-5084 #time 1h 30min #comment Create a hook for managing Modal state

---
+++ src/hooks/useModal/index.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { ModalState } from 'src/hooks/useModal/useModal'
+
+/**
+ * Hook to manage modal state.
+ *
+ * @returns Modal state.
+ */
+const useModal: () => ModalState = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    /**
+     * Opening the modal.
+     */
+    const openModal = () => {
+        setIsOpen(true)
+    }
+
+    /**
+     * Closing the modal.
+     */
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
+    return {
+        isOpen,
+        openModal,
+        closeModal,
+    }
+}
+
+export default useModal
