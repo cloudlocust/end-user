48af8ae67d57c689c3f9a44901afe9e6fd4e92b2
Abdelkadir <ga_bouziane@esi.dz>
2023-09-19 19:51:49 +0100

    MYEM-5084 #time 30m #comment Add tests for the InfosPage component

---
+++ src/modules/MyHouse/components/MicrowaveMeasurement/InfosPage/InfosPage.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import InfosPage from './index'
+
+// Mock the function passed as a prop
+const mockStepSetter = jest.fn()
+
+describe('InfosPage Component', () => {
+    test('renders header correctly', () => {
+        render(<InfosPage stepSetter={mockStepSetter} />)
+
+        const headerText = screen.getByText("Mesure d'appareil")
+        expect(headerText).toBeInTheDocument()
+
+        const subHeaderText = screen.getByText('Micro Onde')
+        expect(subHeaderText).toBeInTheDocument()
+    })
+
+    test('renders test benefits and steps correctly', () => {
+        render(<InfosPage stepSetter={mockStepSetter} />)
+
+        const benefitText = screen.getByText((content, _) => {
+            return content.startsWith('Tester votre appareil vous permet de')
+        })
+        expect(benefitText).toBeInTheDocument()
+
+        const stepText = screen.getByText((content, _) => {
+            return content.startsWith('Le test se déroule en 4 étapes')
+        })
+        expect(stepText).toBeInTheDocument()
+    })
+
+    test('calls stepSetter when the button Commencer is clicked', async () => {
+        render(<InfosPage stepSetter={mockStepSetter} />)
+
+        const button = screen.getByText('Commencer')
+        userEvent.click(button)
+
+        // Check if the stepSetter function was called
+        await waitFor(() => {
+            expect(mockStepSetter).toHaveBeenCalledWith(1)
+        })
+    })
+})
