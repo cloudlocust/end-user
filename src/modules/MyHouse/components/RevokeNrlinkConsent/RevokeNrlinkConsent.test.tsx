d8dbd8e27f3d4c2e8eb0eeffaee0602426a8fe39
Abdelkadir <ga_bouziane@esi.dz>
2023-09-29 15:46:09 +0100

    MYEM-4858 #time 3h #comment Use material-ui-confirm library in the RevokeNrlinkConsent dialog and Add tests for it

--- src/modules/MyHouse/components/RevokeNrlinkConsent/RevokeNrlinkConsent.test.tsx
+++ src/modules/MyHouse/components/RevokeNrlinkConsent/RevokeNrlinkConsent.test.tsx
@@ -3,6 +3,7 @@ import { reduxedRender } from 'src/common/react-platform-components/test'
 import userEvent from '@testing-library/user-event'
 import { INrlinkConsent } from 'src/modules/Consents/Consents.d'
 import { RevokeNrlinkConsent } from 'src/modules/MyHouse/components/RevokeNrlinkConsent'
+import { ConfirmProvider } from 'material-ui-confirm'
 
 const nrLinkConsent: INrlinkConsent = {
     meterGuid: 'meterGuid',
@@ -11,26 +12,53 @@ const nrLinkConsent: INrlinkConsent = {
     createdAt: 'createdAt',
 }
 const onAfterRevokeNRLink = jest.fn()
+const dialogText = 'Vous êtes sur le point de révoquer votre consentement nrLINK. Êtes-vous sûr de vouloir continuer ?'
 
 describe('RevokeNrlinkConsent', () => {
     test('opens the dialog when the delete button is clicked', async () => {
-        reduxedRender(<RevokeNrlinkConsent nrLinkConsent={nrLinkConsent} onAfterRevokeNRLink={onAfterRevokeNRLink} />)
+        reduxedRender(
+            <ConfirmProvider>
+                <RevokeNrlinkConsent nrLinkConsent={nrLinkConsent} onAfterRevokeNRLink={onAfterRevokeNRLink} />,
+            </ConfirmProvider>,
+        )
         const deleteButton = screen.getByRole('button')
         userEvent.click(deleteButton)
         await waitFor(() => {
-            expect(screen.getByRole('dialog')).toBeInTheDocument()
+            expect(screen.getByText(dialogText)).toBeInTheDocument()
         })
     })
 
-    test('closes the dialog when the Non button is clicked', async () => {
-        reduxedRender(<RevokeNrlinkConsent nrLinkConsent={nrLinkConsent} onAfterRevokeNRLink={onAfterRevokeNRLink} />)
+    test('closes the dialog when the button Annuler is clicked', async () => {
+        reduxedRender(
+            <ConfirmProvider>
+                <RevokeNrlinkConsent nrLinkConsent={nrLinkConsent} onAfterRevokeNRLink={onAfterRevokeNRLink} />
+            </ConfirmProvider>,
+        )
         const deleteButton = screen.getByRole('button')
         userEvent.click(deleteButton)
         await waitFor(async () => {
-            const nonButton = screen.getByRole('button', { name: 'Annuler' })
-            userEvent.click(nonButton)
+            const annulerButton = screen.getByRole('button', { name: 'Annuler' })
+            userEvent.click(annulerButton)
             await waitFor(() => {
-                expect(screen.queryByRole('dialog')).toBeNull()
+                expect(screen.queryByText(dialogText)).toBeNull()
+            })
+        })
+    })
+
+    test('closes the dialog and call the onAfterRevokeNRLink function when the button Continuer is clicked', async () => {
+        reduxedRender(
+            <ConfirmProvider>
+                <RevokeNrlinkConsent nrLinkConsent={nrLinkConsent} onAfterRevokeNRLink={onAfterRevokeNRLink} />
+            </ConfirmProvider>,
+        )
+        const deleteButton = screen.getByRole('button')
+        userEvent.click(deleteButton)
+        await waitFor(async () => {
+            const continuerButton = screen.getByRole('button', { name: 'Continuer' })
+            userEvent.click(continuerButton)
+            await waitFor(() => {
+                expect(screen.queryByText(dialogText)).toBeNull()
+                expect(onAfterRevokeNRLink).toHaveBeenCalledTimes(1)
             })
         })
     })
--- src/modules/MyHouse/components/RevokeNrlinkConsent/index.tsx
+++ src/modules/MyHouse/components/RevokeNrlinkConsent/index.tsx
@@ -1,19 +1,13 @@
 import IconButton from '@mui/material/IconButton'
-import Dialog from '@mui/material/Dialog'
-import DialogTitle from '@mui/material/DialogTitle'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogActions from '@mui/material/DialogActions'
-import Button from '@mui/material/Button'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import { useSelector } from 'react-redux'
 import { RootState } from 'src/redux'
-import { useModal } from 'src/hooks/useModal'
-import { useIntl } from 'src/common/react-platform-translation'
-import { ButtonLoader } from 'src/common/ui-kit'
 import { useConsents } from 'src/modules/Consents/consentsHook'
 import { RevokeNrlinkConsentProps } from 'src/modules/MyHouse/components/RevokeNrlinkConsent/RevokeNrlinkConsent'
-import Paper from '@mui/material/Paper'
+import { useTheme } from '@mui/material/styles'
+import { useConfirm } from 'material-ui-confirm'
+import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
+import { useIntl } from 'src/common/react-platform-translation'
 
 /**
  * Revoke NRLink Consent Module.
@@ -24,78 +18,55 @@ import Paper from '@mui/material/Paper'
  * @returns NRLink Revoke Consent Component.
  */
 export const RevokeNrlinkConsent = ({ nrLinkConsent, onAfterRevokeNRLink }: RevokeNrlinkConsentProps) => {
+    const theme = useTheme()
     const { formatMessage } = useIntl()
+    const openMuiDialog = useConfirm()
     const { currentHousing } = useSelector(({ housingModel }: RootState) => housingModel)
-    const { revokeNrlinkConsent, isNrlinkConsentLoading } = useConsents()
-    const { isOpen, openModal: openDialog, closeModal: closeDialog } = useModal()
+    const { revokeNrlinkConsent } = useConsents()
 
     /**
      * Function for revoking NRLink consent.
      */
     const handleRevokeNRLinkConsent = async () => {
         try {
+            await openMuiDialog({
+                title: '',
+                dialogProps: {
+                    PaperProps: {
+                        style: {
+                            background: theme.palette.error.main,
+                        },
+                    },
+                },
+                description: (
+                    <TypographyFormatMessage className="text-16 md:text-20 text-center text-white">
+                        Vous êtes sur le point de révoquer votre consentement nrLINK. Êtes-vous sûr de vouloir continuer
+                        ?
+                    </TypographyFormatMessage>
+                ),
+                confirmationText: formatMessage({
+                    id: 'Continuer',
+                    defaultMessage: 'Continuer',
+                }),
+                confirmationButtonProps: {
+                    className: 'text-13 md:text-16 font-medium text-white',
+                },
+                cancellationText: formatMessage({
+                    id: 'Annuler',
+                    defaultMessage: 'Annuler',
+                }),
+                cancellationButtonProps: {
+                    className: 'text-13 md:text-16 font-medium text-white',
+                },
+            })
             await revokeNrlinkConsent(currentHousing?.id, nrLinkConsent?.nrlinkGuid)
             onAfterRevokeNRLink()
         } catch {}
-        closeDialog()
     }
 
     return (
-        <>
-            <IconButton color="error" size="small" onClick={openDialog}>
-                <DeleteOutlinedIcon />
-            </IconButton>
-            <Dialog
-                open={isOpen}
-                onClose={closeDialog}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <Paper sx={{ backgroundColor: 'red' }}>
-                    <DialogTitle id="alert-dialog-title" className="text-white text-center">
-                        {formatMessage({
-                            id: 'Révocation du consentement nrLINK !',
-                            defaultMessage: 'Révocation du consentement nrLINK !',
-                        })}
-                    </DialogTitle>
-                    <DialogContent sx={{ paddingY: 0, marginTop: '20px', marginBottom: '10px' }}>
-                        <DialogContentText
-                            id="alert-dialog-description"
-                            className="text-white text-center text-sm font-medium"
-                        >
-                            {formatMessage({
-                                id: 'Vous êtes sur le point de révoquer votre consentement nrLINK. Êtes-vous sûr de vouloir continuer ?',
-                                defaultMessage:
-                                    'Vous êtes sur le point de révoquer votre consentement nrLINK. Êtes-vous sûr de vouloir continuer ?',
-                            })}
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions className="flex items-center justify-center">
-                        <Button
-                            variant="outlined"
-                            className="text-white m-12 border-white"
-                            onClick={closeDialog}
-                            autoFocus
-                        >
-                            {formatMessage({
-                                id: 'Annuler',
-                                defaultMessage: 'Annuler',
-                            })}
-                        </Button>
-                        <ButtonLoader
-                            variant="outlined"
-                            className="text-white m-12 border-white"
-                            onClick={handleRevokeNRLinkConsent}
-                            inProgress={isNrlinkConsentLoading}
-                        >
-                            {formatMessage({
-                                id: 'Continuer',
-                                defaultMessage: 'Continuer',
-                            })}
-                        </ButtonLoader>
-                    </DialogActions>
-                </Paper>
-            </Dialog>
-        </>
+        <IconButton color="error" size="small" onClick={handleRevokeNRLinkConsent}>
+            <DeleteOutlinedIcon />
+        </IconButton>
     )
 }
