04cc373100a644f4d24d836ed41ed946ef89c746
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-10-16 16:30:04 +0100

    MYEM-5184 #time 1h #comment Adjust code after review

--- src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/MeasurementProcessStep.test.tsx
+++ src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/MeasurementProcessStep.test.tsx
@@ -1,63 +1,24 @@
-import { Theme } from '@mui/material'
 import { screen } from '@testing-library/react'
 import { reduxedRender } from 'src/common/react-platform-components/test'
-import {
-    ResponseMessage,
-    MeasurementProcessStep,
-} from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep'
-
-const TEST_TITLE = 'test title'
-const TEST_CONTENT = 'test content'
-const SUCCESS_COLOR = 'rgb(0, 255, 0)'
-const ERROR_COLOR = 'rgb(255, 0, 0)'
-
-const ResponseMessageProps = {
-    theme: {
-        palette: {
-            success: { main: SUCCESS_COLOR },
-            error: { main: ERROR_COLOR },
-        },
-    } as Theme,
-    title: TEST_TITLE,
-    content: TEST_CONTENT,
-}
+import { MeasurementProcessStep } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep'
+import { MeasurementProcessStepProps } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/MeasurementProcessStep'
 
 // Mock StepSetterFunction
-const mockStepSetter = jest.fn()
-
-const MeasurementProcessStepProps = {
-    microwave: '',
-    measurementMode: '',
-    stepSetter: mockStepSetter,
-}
-
-describe('ResponseMessage Component', () => {
-    test('Renders correctly', () => {
-        reduxedRender(<ResponseMessage {...ResponseMessageProps} />)
-
-        const titleElement = screen.getByText(TEST_TITLE)
-        expect(titleElement).toBeInTheDocument()
-
-        // Title must have the color error of MUI theme if the success property is false (not specified)
-        const computedStyle = window.getComputedStyle(titleElement)
-        expect(computedStyle.color).toBe(ERROR_COLOR)
-
-        const contentElement = screen.getByText(TEST_CONTENT)
-        expect(contentElement).toBeInTheDocument()
-    })
-
-    test('Title color is the success color of MUI theme when the success property is true', () => {
-        reduxedRender(<ResponseMessage {...ResponseMessageProps} success />)
-
-        const titleElement = screen.getByText(TEST_TITLE)
-        const computedStyle = window.getComputedStyle(titleElement)
-        expect(computedStyle.color).toBe(SUCCESS_COLOR)
-    })
-})
+let mockStepSetter: jest.Mock<any, any>
+let MeasurementProcessStepPropsValues: MeasurementProcessStepProps
 
 describe('MeasurementProcessStep Component', () => {
+    beforeEach(() => {
+        mockStepSetter = jest.fn()
+        MeasurementProcessStepPropsValues = {
+            microwave: '',
+            measurementMode: '',
+            stepSetter: mockStepSetter,
+        }
+    })
+
     test('Renders correctly', () => {
-        reduxedRender(<MeasurementProcessStep {...MeasurementProcessStepProps} />)
+        reduxedRender(<MeasurementProcessStep {...MeasurementProcessStepPropsValues} />)
 
         const header = screen.getByTestId('headerElement')
         expect(header).toBeInTheDocument()
--- src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/index.tsx
+++ src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/index.tsx
@@ -4,47 +4,18 @@ import Button from '@mui/material/Button'
 import { useTheme } from '@mui/material'
 import { useIntl } from 'src/common/react-platform-translation'
 import { measurementStatusEnum } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProgress/MeasurementProgress.d'
-import {
-    MeasurementProcessStepProps,
-    ResponseMessageProps,
-} from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/MeasurementProcessStep'
+import { MeasurementProcessStepProps } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/MeasurementProcessStep'
 import { MeasurementProgress } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProgress'
-import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
-
-/**
- * Component for showing the response message of the measurement.
- *
- * @param root0 N/A.
- * @param root0.theme The MUI Theme object.
- * @param root0.title The title of the message.
- * @param root0.content The content of the message.
- * @param root0.success True if it's a success response.
- * @returns The ResponseMessage component.
- */
-export const ResponseMessage = ({ theme, title, content, success }: ResponseMessageProps) => (
-    <div className="text-center">
-        <TypographyFormatMessage
-            fontWeight="500"
-            fontSize="15px"
-            marginBottom="5px"
-            color={success ? theme.palette.success.main : theme.palette.error.main}
-        >
-            {title}
-        </TypographyFormatMessage>
-        <TypographyFormatMessage fontSize="14px">{content}</TypographyFormatMessage>
-    </div>
-)
+import { ResponseMessage } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementProcessStep/ResponseMessage'
 
 /**
  * MeasurementProcessStep component.
  *
  * @param root0 N/A.
- * @param root0.microwave The microwave to mesure.
- * @param root0.measurementMode The measurement mode.
  * @param root0.stepSetter The setter linked to the state responsible for storing the current step.
  * @returns The MeasurementProcessStep component.
  */
-export const MeasurementProcessStep = ({ microwave, measurementMode, stepSetter }: MeasurementProcessStepProps) => {
+export const MeasurementProcessStep = ({ stepSetter }: MeasurementProcessStepProps) => {
     const { formatMessage } = useIntl()
     const theme = useTheme()
     const [measurementStatus, setMeasurementStatus] = useState<measurementStatusEnum>(measurementStatusEnum.pending)
@@ -104,60 +75,58 @@ export const MeasurementProcessStep = ({ microwave, measurementMode, stepSetter 
             </div>
 
             {/* Content */}
-            <div>
-                <div className="min-h-256 flex flex-col justify-around">
-                    {/* The measurement progress component */}
-                    <div className="flex justify-center">
-                        <MeasurementProgress status={measurementStatus} maxDuration={measurementMaxDuration} />
-                    </div>
-
-                    {/* Success message */}
-                    {measurementStatus === measurementStatusEnum.success && (
-                        <ResponseMessage
-                            title="Félicitations !"
-                            content="Le test s'est terminé avec succès, vous pouvez désormais analyser vos résultats"
-                            theme={theme}
-                            success
-                        />
-                    )}
-
-                    {/* Failure message */}
-                    {measurementStatus === measurementStatusEnum.failed && (
-                        <ResponseMessage
-                            title="La mesure a échoué"
-                            content="Le test s'est terminé par un échec, vous pouvez le lancer à nouveau"
-                            theme={theme}
-                        />
-                    )}
+            <div className="min-h-256 flex flex-col justify-around">
+                {/* The measurement progress component */}
+                <div className="flex justify-center">
+                    <MeasurementProgress status={measurementStatus} maxDuration={measurementMaxDuration} />
                 </div>
 
-                {/* The test ending button */}
-                <div className="flex justify-center mt-20">
-                    {measurementStatus !== measurementStatusEnum.failed ? (
-                        <Button
-                            variant="contained"
-                            sx={{ padding: '10px auto', textAlign: 'center', width: '60%', minWidth: '160px' }}
-                            onClick={handleFinishBtnClick}
-                            disabled={measurementStatus !== measurementStatusEnum.success}
-                        >
-                            {formatMessage({
-                                id: 'Terminer',
-                                defaultMessage: 'Terminer',
-                            })}
-                        </Button>
-                    ) : (
-                        <Button
-                            variant="contained"
-                            sx={{ padding: '10px auto', textAlign: 'center', width: '60%', minWidth: '160px' }}
-                            onClick={restartTest}
-                        >
-                            {formatMessage({
-                                id: 'Relancer le test',
-                                defaultMessage: 'Relancer le test',
-                            })}
-                        </Button>
-                    )}
-                </div>
+                {/* Success message */}
+                {measurementStatus === measurementStatusEnum.success && (
+                    <ResponseMessage
+                        title="Félicitations !"
+                        content="Le test s'est terminé avec succès, vous pouvez désormais analyser vos résultats"
+                        theme={theme}
+                        success
+                    />
+                )}
+
+                {/* Failure message */}
+                {measurementStatus === measurementStatusEnum.failed && (
+                    <ResponseMessage
+                        title="La mesure a échoué"
+                        content="Le test s'est terminé par un échec, vous pouvez le lancer à nouveau"
+                        theme={theme}
+                    />
+                )}
+            </div>
+
+            {/* The test ending button */}
+            <div className="flex justify-center mt-20">
+                {measurementStatus !== measurementStatusEnum.failed ? (
+                    <Button
+                        variant="contained"
+                        sx={{ padding: '10px auto', textAlign: 'center', width: '60%', minWidth: '160px' }}
+                        onClick={handleFinishBtnClick}
+                        disabled={measurementStatus !== measurementStatusEnum.success}
+                    >
+                        {formatMessage({
+                            id: 'Terminer',
+                            defaultMessage: 'Terminer',
+                        })}
+                    </Button>
+                ) : (
+                    <Button
+                        variant="contained"
+                        sx={{ padding: '10px auto', textAlign: 'center', width: '60%', minWidth: '160px' }}
+                        onClick={restartTest}
+                    >
+                        {formatMessage({
+                            id: 'Relancer le test',
+                            defaultMessage: 'Relancer le test',
+                        })}
+                    </Button>
+                )}
             </div>
         </>
     )
