2cccfadf4bc085e6247e8f045aba7e9f252f6bc2
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-10-30 22:12:15 +0100

    MYEM-5285 #time 20m  #comment Simplify HistogramBar tests

--- src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementComparisonHistogram/HistogramBar.test.tsx
+++ src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementComparisonHistogram/HistogramBar.test.tsx
@@ -8,59 +8,62 @@ describe('HistogramBarIcon component', () => {
     test('renders the icon correctly', () => {
         const { getByTestId } = render(<HistogramBarIcon icon={<></>} />)
 
-        const iconElement = getByTestId('histogram-bar-icon')
-        expect(iconElement).toBeInTheDocument()
+        // Assert that the HistogramBar Icon is present
+        expect(getByTestId('histogram-bar-icon')).toBeInTheDocument()
     })
 })
 
 const HISTOGRAM_BAR_TEST_ID = 'histogram-bar'
 
 describe('HistogramBar component', () => {
-    test('renders correctly when the HistogramBar is for the user consumption', () => {
-        const { getByText, getByTestId } = render(<HistogramBar consumptionValue={800} otherConsumptionValue={1000} />)
-
-        // Assert that the consumption value is present
-        const consumptionValueElement = getByText('800 W')
-        expect(consumptionValueElement).toBeInTheDocument()
-
-        // Assert that the person icon is present
-        const personIcon = getByTestId('PersonOutlineOutlinedIcon')
-        expect(personIcon).toBeInTheDocument()
-
-        // Assert that the height of the bar is calculated correctly
-        const barElement = getByTestId(HISTOGRAM_BAR_TEST_ID)
-        expect(barElement).toHaveStyle({
-            height: '80%',
-        })
-    })
-
     test('renders correctly when the HistogramBar is for the average consumption', () => {
         const { getByText, getByTestId } = render(
             <HistogramBar consumptionValue={600} otherConsumptionValue={1000} isAverageConsumption />,
         )
 
-        // Assert that the consumption value is present
-        const consumptionValueElement = getByText('600 W')
-        expect(consumptionValueElement).toBeInTheDocument()
+        // Assert that the consumption value is present and correct
+        expect(getByText('600 W')).toBeInTheDocument()
 
         // Assert that the group icon is present
-        const groupIcon = getByTestId('GroupsOutlinedIcon')
-        expect(groupIcon).toBeInTheDocument()
+        expect(getByTestId('GroupsOutlinedIcon')).toBeInTheDocument()
 
-        // Assert that the height of the bar is calculated correctly
-        const barElement = getByTestId(HISTOGRAM_BAR_TEST_ID)
-        expect(barElement).toHaveStyle({
+        // Assert that the bar height and background color are correct
+        expect(getByTestId(HISTOGRAM_BAR_TEST_ID)).toHaveStyle({
             height: '60%',
+            backgroundColor: 'transparent',
+        })
+    })
+
+    test('renders correctly when the HistogramBar is for the user consumption', () => {
+        const { getByText, getByTestId } = render(<HistogramBar consumptionValue={800} otherConsumptionValue={1000} />)
+
+        // Assert that the consumption value is present and correct
+        expect(getByText('800 W')).toBeInTheDocument()
+
+        // Assert that the person icon is present
+        expect(getByTestId('PersonOutlineOutlinedIcon')).toBeInTheDocument()
+
+        // Assert that the bar height and background color are correct
+        const histogramBar = getByTestId(HISTOGRAM_BAR_TEST_ID)
+        expect(histogramBar).toHaveStyle({
+            height: '80%',
+        })
+        expect(histogramBar).not.toHaveStyle({
+            height: 'transparent',
         })
     })
 
     test('the height of the bar must be 100% when the consumptionValue is greater than or equal to the otherConsumptionValue', () => {
         const { getByTestId, rerender } = render(<HistogramBar consumptionValue={1400} otherConsumptionValue={50} />)
+
+        // Assert that the height of the bar is equal to 100%
         expect(getByTestId(HISTOGRAM_BAR_TEST_ID)).toHaveStyle({
             height: '100%',
         })
 
         rerender(<HistogramBar consumptionValue={5800} otherConsumptionValue={5800} />)
+
+        // Assert that the height of the bar is equal to 100%
         expect(getByTestId(HISTOGRAM_BAR_TEST_ID)).toHaveStyle({
             height: '100%',
         })
