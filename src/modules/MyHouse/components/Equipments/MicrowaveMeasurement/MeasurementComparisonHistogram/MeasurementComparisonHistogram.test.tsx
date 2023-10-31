4f07d487b991aaebf9dedc5fd48481c40b0ed997
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-10-31 13:32:44 +0100

    MYEM-5290 #time 10m  #comment Adjust the tests of the MeasurementComparisonHistogram component

--- src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementComparisonHistogram/MeasurementComparisonHistogram.test.tsx
+++ src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementComparisonHistogram/MeasurementComparisonHistogram.test.tsx
@@ -2,12 +2,18 @@ import { reduxedRender } from 'src/common/react-platform-components/test'
 import { MeasurementComparisonHistogram } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MeasurementComparisonHistogram'
 
 const HISTOGRAM_BAR_TEST_ID = 'histogram-bar'
+const USER_CONSUMPTION_LABEL_TEXT = 'Consommation de votre micro onde'
+const AVERAGE_CONSUMPTION_LABEL_TEXT = "Consommation moyenne d'un micro onde"
 
 describe('MeasurementComparisonHistogram component', () => {
     test('renders correctly when the averageConsumption is greater than the userConsumption', () => {
         const { getByText, getByTestId, getAllByTestId } = reduxedRender(
             <MeasurementComparisonHistogram userConsumption={750} averageConsumption={1000} />,
         )
+
+        // Assert that the consumptions labels are present
+        expect(getByText(USER_CONSUMPTION_LABEL_TEXT)).toBeInTheDocument()
+        expect(getByText(AVERAGE_CONSUMPTION_LABEL_TEXT)).toBeInTheDocument()
 
         // Assert that the consumptions values are present and correct
         expect(getByText('750 W')).toBeInTheDocument()
