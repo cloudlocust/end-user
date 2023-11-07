056373a7958f8743c3ca1138da8a34da7d2b7a12
Amine Belmahi <amine@myem.fr>
2023-11-07 04:51:32 +0100

    MYEM-5307 #time 4h #comment fix css when select value is longer

--- src/modules/Layout/Toolbar/components/SelectHousing/index.tsx
+++ src/modules/Layout/Toolbar/components/SelectHousing/index.tsx
@@ -2,7 +2,18 @@ import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Dispatch, RootState } from 'src/redux'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
-import { MenuItem, Input, Divider, Tooltip, Button, SvgIcon, ListSubheader, Backdrop } from '@mui/material'
+import {
+    MenuItem,
+    Input,
+    Divider,
+    Tooltip,
+    Button,
+    SvgIcon,
+    ListSubheader,
+    Backdrop,
+    useTheme,
+    useMediaQuery,
+} from '@mui/material'
 import { useIntl } from 'react-intl'
 import { Link } from 'react-router-dom'
 import TypographyFormatMessage from 'src/common/ui-kit/components/TypographyFormatMessage/TypographyFormatMessage'
@@ -33,6 +44,8 @@ const MenuProps = {
  */
 export const SelectHousing = () => {
     const { formatMessage } = useIntl()
+    const theme = useTheme()
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'))
     // We use the dispatch to get the housing model from the redux state.
     const dispatch = useDispatch<Dispatch>()
     const { housingList, currentHousing } = useSelector(({ housingModel }: RootState) => housingModel)
@@ -53,68 +66,72 @@ export const SelectHousing = () => {
     }
     return (
         <>
-            <Select
-                displayEmpty={!currentHousing?.address.name}
-                labelId="select-housing-label"
-                id="select-housing"
-                value={currentHousing?.id ?? ''}
-                onChange={handleChange}
-                input={<Input disableUnderline />}
-                renderValue={() => {
-                    return (
-                        <div className="underline">
-                            {!currentHousing?.address.name
-                                ? 'Aucun logement disponible'
-                                : currentHousing?.address?.name}
-                        </div>
-                    )
-                }}
-                defaultValue={'Aucun logement disponible'}
-                MenuProps={MenuProps}
-                onOpen={() => setOpenBackdrop(true)}
-                onClose={() => setOpenBackdrop(false)}
-            >
-                {housingList?.map((housing) => (
-                    <MenuItem key={housing.id} value={housing.id} className="flex justify-between">
-                        <TypographyFormatMessage className="mr-8" noWrap>
-                            {housing.address.name}
-                        </TypographyFormatMessage>
-                        <Link to={`/my-houses/${housing.id}`}>
-                            <SettingsIcon sx={{ color: 'grey.600' }} />
-                        </Link>
-                    </MenuItem>
-                ))}
-                <ListSubheader>
-                    <Divider className="my-8" />
-                    <Tooltip
-                        arrow
-                        placement="top"
-                        disableHoverListener={!deleteAddFeatureState}
-                        title={formatMessage({
-                            id: "Cette fonctionnalité n'est pas encore disponible",
-                            defaultMessage: "Cette fonctionnalité n'est pas encore disponible",
-                        })}
-                    >
-                        <div className={`flex justify-center ${deleteAddFeatureState && 'cursor-not-allowed'}`}>
-                            <Button
-                                variant="text"
-                                disabled={deleteAddFeatureState}
-                                startIcon={
-                                    <SvgIcon color={deleteAddFeatureState ? 'disabled' : 'inherit'}>
-                                        <AddIcon />
-                                    </SvgIcon>
-                                }
-                                onClick={() => setModalAddHousingOpen(true)}
-                            >
-                                {formatMessage({
-                                    id: 'Ajouter un logement',
-                                    defaultMessage: 'Ajouter un logement',
-                                })}
-                            </Button>
-                        </div>
-                    </Tooltip>{' '}
-                </ListSubheader>
-            </Select>
+            <div className="w-full flex justify-start items-center truncate">
+                <Select
+                    className="w-full flex justify-center items-center"
+                    displayEmpty={!currentHousing?.address.name}
+                    labelId="select-housing-label"
+                    id="select-housing"
+                    value={currentHousing?.id ?? ''}
+                    onChange={handleChange}
+                    input={<Input disableUnderline />}
+                    sx={{ maxWidth: smDown ? '100%' : '250px' }}
+                    renderValue={() => {
+                        return (
+                            <div className="underline truncate">
+                                {!currentHousing?.address.name
+                                    ? 'Aucun logement disponible'
+                                    : currentHousing?.address?.name}
+                            </div>
+                        )
+                    }}
+                    defaultValue={'Aucun logement disponible'}
+                    MenuProps={MenuProps}
+                    onOpen={() => setOpenBackdrop(true)}
+                    onClose={() => setOpenBackdrop(false)}
+                >
+                    {housingList?.map((housing) => (
+                        <MenuItem key={housing.id} value={housing.id} className="flex justify-between">
+                            <TypographyFormatMessage className="mr-8 whitespace-normal">
+                                {housing.address.name}
+                            </TypographyFormatMessage>
+                            <Link to={`/my-houses/${housing.id}`}>
+                                <SettingsIcon sx={{ color: 'grey.600' }} />
+                            </Link>
+                        </MenuItem>
+                    ))}
+                    <ListSubheader>
+                        <Divider className="my-8" />
+                        <Tooltip
+                            arrow
+                            placement="top"
+                            disableHoverListener={!deleteAddFeatureState}
+                            title={formatMessage({
+                                id: "Cette fonctionnalité n'est pas encore disponible",
+                                defaultMessage: "Cette fonctionnalité n'est pas encore disponible",
+                            })}
+                        >
+                            <div className={`flex justify-center ${deleteAddFeatureState && 'cursor-not-allowed'}`}>
+                                <Button
+                                    variant="text"
+                                    disabled={deleteAddFeatureState}
+                                    startIcon={
+                                        <SvgIcon color={deleteAddFeatureState ? 'disabled' : 'inherit'}>
+                                            <AddIcon />
+                                        </SvgIcon>
+                                    }
+                                    onClick={() => setModalAddHousingOpen(true)}
+                                >
+                                    {formatMessage({
+                                        id: 'Ajouter un logement',
+                                        defaultMessage: 'Ajouter un logement',
+                                    })}
+                                </Button>
+                            </div>
+                        </Tooltip>
+                    </ListSubheader>
+                </Select>
+            </div>
             <Backdrop open={openBackdrop} onClick={() => setOpenBackdrop(true)} />
             <AddHousingModal modalOpen={modalAddHousingOpen} closeModal={() => setModalAddHousingOpen(false)} />
         </>
