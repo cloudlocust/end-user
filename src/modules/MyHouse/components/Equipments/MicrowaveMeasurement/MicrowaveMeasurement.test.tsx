b9624677e295d48242c29badc2e1c0f09095ed98
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-11-14 18:50:08 +0100

    MYEM-5358 #time 5m #comment Fix measurement popup tests

--- src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MicrowaveMeasurement.test.tsx
+++ src/modules/MyHouse/components/Equipments/MicrowaveMeasurement/MicrowaveMeasurement.test.tsx
@@ -3,22 +3,25 @@ import userEvent from '@testing-library/user-event'
 import { reduxedRender } from 'src/common/react-platform-components/test'
 import { MicrowaveMeasurement } from 'src/modules/MyHouse/components/Equipments/MicrowaveMeasurement'
 
+const mockOnCloseMeasurementModal = jest.fn()
+const mockNavigateToEquipmentDetailsPage = jest.fn()
+
 describe('MicrowaveMeasurement tests', () => {
     test('renders the MicrowaveMeasurement modal and close it', async () => {
-        const onCloseModalMock = jest.fn()
         reduxedRender(
             <MicrowaveMeasurement
                 housingEquipmentId={25}
                 equipmentsNumber={1}
                 measurementModes={['A', 'B']}
                 isMeasurementModalOpen={true}
-                onCloseMeasurementModal={onCloseModalMock}
+                onCloseMeasurementModal={mockOnCloseMeasurementModal}
+                navigateToEquipmentDetailsPage={mockNavigateToEquipmentDetailsPage}
             />,
         )
 
         userEvent.click(screen.getByRole('button', { name: 'close' }))
         await waitFor(() => {
-            expect(onCloseModalMock).toHaveBeenCalledTimes(1)
+            expect(mockOnCloseMeasurementModal).toHaveBeenCalledTimes(1)
         })
     })
 })
