217c6ab5b3e612c03546b89e8fcda1d72483329b
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-11-14 15:23:18 +0100

    MYEM-5358 #time 30m #comment Add tests for the EquipmentDetailsHeader component

---
+++ src/modules/MyHouse/components/EquipmentDetails/EquipmentDetailsHeader/EquipmentDetailsHeader.test.tsx
@@ -0,0 +1,40 @@
+import { waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { reduxedRender } from 'src/common/react-platform-components/test'
+import { EquipmentDetailsHeader } from 'src/modules/MyHouse/components/EquipmentDetails/EquipmentDetailsHeader'
+import { myEquipmentOptions } from 'src/modules/MyHouse/utils/MyHouseVariables'
+
+let mockHistoryGoBack = jest.fn()
+let EQUIPMENT_NAME = myEquipmentOptions[0].name
+let EQUIPMENT_LABEL_TITLE = myEquipmentOptions[0].labelTitle
+let GO_BACK_BUTTON_TEXT = 'Retour'
+
+jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router'),
+
+    // eslint-disable-next-line jsdoc/require-jsdoc
+    useHistory: () => ({
+        goBack: mockHistoryGoBack,
+        listen: jest.fn(),
+    }),
+}))
+
+describe('EquipmentDetailsHeader tests', () => {
+    test('renders elements correctly', async () => {
+        const { getByText } = reduxedRender(<EquipmentDetailsHeader equipmentName={EQUIPMENT_NAME} />)
+
+        expect(getByText(GO_BACK_BUTTON_TEXT)).toBeInTheDocument()
+        expect(getByText('arrow_back')).toBeInTheDocument()
+        expect(getByText(EQUIPMENT_LABEL_TITLE)).toBeInTheDocument()
+    })
+
+    test('call history.goBack when the button Retour clicked', async () => {
+        const { getByText } = reduxedRender(<EquipmentDetailsHeader equipmentName={EQUIPMENT_NAME} />)
+
+        expect(getByText(GO_BACK_BUTTON_TEXT)).toBeInTheDocument()
+        userEvent.click(getByText(GO_BACK_BUTTON_TEXT))
+        await waitFor(() => {
+            expect(mockHistoryGoBack).toHaveBeenCalled()
+        })
+    })
+})
