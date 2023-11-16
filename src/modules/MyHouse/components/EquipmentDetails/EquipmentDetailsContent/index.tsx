b1c18ba7526c22834d9db97f1c275c58d3978c93
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-11-16 17:24:24 +0100

    MYEM-5406 #time 6h  #comment Create the EquipmentDetailsContent component

---
+++ src/modules/MyHouse/components/EquipmentDetails/EquipmentDetailsContent/index.tsx
@@ -0,0 +1,112 @@
+import { useIntl } from 'src/common/react-platform-translation'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import {
+    EquipmentDetailsContentProps,
+    SelectOnChangeHandler,
+} from 'src/modules/MyHouse/components/EquipmentDetails/EquipmentDetailsContent/EquipmentDetailsContent'
+import { useState } from 'react'
+import { myEquipmentOptions } from 'src/modules/MyHouse/utils/MyHouseVariables'
+import { EquipmentMeasurementResultsList } from 'src/modules/MyHouse/components/EquipmentDetails/EquipmentMeasurementResultsList'
+
+/**
+ * EquipmentDetailsContent compoonent.
+ *
+ * @param root0 N/A.
+ * @param root0.equipmentDetails The equipment details object.
+ * @returns EquipmentDetailsContent JSX.
+ */
+export const EquipmentDetailsContent = ({ equipmentDetails }: EquipmentDetailsContentProps) => {
+    const { formatMessage } = useIntl()
+    const max_width_600 = useMediaQuery('(max-width:600px)')
+    const max_width_470 = useMediaQuery('(max-width:470px)')
+    const [selectedEquipmentNumber, setSelectedEquipmentNumber] = useState(equipmentDetails.number === 1 ? 1 : null)
+    const myEquipmentStr = 'Mon équipement'
+    const { labelTitle: equipmentLabel } =
+        myEquipmentOptions.find((element) => element.name === equipmentDetails.name) || {}
+
+    /**
+     * The select onChange handler function.
+     *
+     * @param event The onChange event.
+     */
+    const handleSelectEquipmentChange: SelectOnChangeHandler = (event) => {
+        setSelectedEquipmentNumber(event.target.value)
+    }
+
+    return (
+        <div className="flex flex-col h-full p-16">
+            {/* The equipment select input */}
+            {equipmentDetails.number && equipmentDetails.number > 1 && (
+                <div className="w-full max-w-640 mx-auto mb-36">
+                    <FormControl fullWidth>
+                        <InputLabel
+                            id="equipment-select-label"
+                            sx={{
+                                fontSize: max_width_600 ? 14 : 16,
+                            }}
+                        >
+                            {formatMessage({
+                                id: myEquipmentStr,
+                                defaultMessage: myEquipmentStr,
+                            })}
+                        </InputLabel>
+                        <Select
+                            labelId="equipment-select-label"
+                            id="equipment-select"
+                            value={selectedEquipmentNumber}
+                            label={formatMessage({
+                                id: myEquipmentStr,
+                                defaultMessage: myEquipmentStr,
+                            })}
+                            onChange={handleSelectEquipmentChange}
+                            sx={{
+                                fontSize: max_width_600 ? 14 : 16,
+                            }}
+                        >
+                            {Array(equipmentDetails.number)
+                                .fill(null)
+                                .map((_, index) => (
+                                    <MenuItem
+                                        value={index + 1}
+                                        sx={{
+                                            fontSize: max_width_600 ? 14 : 16,
+                                        }}
+                                    >{`${equipmentLabel} ${index + 1}`}</MenuItem>
+                                ))}
+                        </Select>
+                    </FormControl>
+                </div>
+            )}
+
+            {/* Measurement result list */}
+            <div className="flex-1">
+                <EquipmentMeasurementResultsList
+                    measurementModes={equipmentDetails.measurementModes}
+                    housingEquipmentId={equipmentDetails.housingEquipmentId}
+                    equipmentNumber={selectedEquipmentNumber}
+                />
+            </div>
+
+            {/* Buttons */}
+            <div className={`flex ${max_width_470 ? 'flex-col' : 'flex-row'} justify-end gap-10 flex-wrap`}>
+                <Button variant="outlined" size="large" className={max_width_600 ? 'flex-1' : ''} disabled>
+                    {formatMessage({
+                        id: "Supprimer l'appareil",
+                        defaultMessage: "Supprimer l'appareil",
+                    })}
+                </Button>
+                <Button variant="contained" size="large" className={max_width_600 ? 'flex-1' : ''}>
+                    {formatMessage({
+                        id: "Mesurer l'appareil",
+                        defaultMessage: "Mesurer l'appareil",
+                    })}
+                </Button>
+            </div>
+        </div>
+    )
+}
