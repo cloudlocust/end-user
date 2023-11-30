bc32011820c3b7d8e3fc626c8e9e06092d5c5e72
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-11-30 12:26:57 +0100

    MYEM-5445 #time 1h 30m #comment Create the ConsumptionAndPrice component

---
+++ src/modules/Dashboard/DashboardConsumptionWidget/ConsumptionAndPrice/ConsumptionAndPrice.d.ts
@@ -0,0 +1,21 @@
+/**
+ * ConsumptionAndPrice props.
+ */
+export interface ConsumptionAndPriceProps {
+    /**
+     * The consumption value.
+     */
+    consumptionValue: number
+    /**
+     * The consumption value unit.
+     */
+    consumptionUnit?: string
+    /**
+     * The price value.
+     */
+    priceValue: number
+    /**
+     * The price value unit.
+     */
+    priceUnit?: string
+}
