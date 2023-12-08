8d058cff4deb076a858d06a7ecd31fb24f13fe1e
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-12-08 10:58:28 +0100

    MYEM-5522 #time 15m #comment Change null values to be 0 in serieValues of conso Widget chart

--- src/modules/Dashboard/DashboardConsumptionWidget/utils.ts
+++ src/modules/Dashboard/DashboardConsumptionWidget/utils.ts
@@ -14,7 +14,9 @@ export const createDataForConsumptionWidgetGraph = (data: IMetric[]) => {
     const { values, timestamps } = formatMetricsDataToTimestampsValues(data)
     const xAxisTimestamps = Object.values(timestamps).length ? Object.values(timestamps)[0] : [0]
     const labels: string[] = getXAxisCategoriesData(xAxisTimestamps, 'daily')
-    const serieValues: number[] = Object.values(values).length ? Object.values(values)[0] : [0]
+    const serieValues: number[] = Object.values(values).length
+        ? Object.values(values)[0].map((value) => value ?? 0)
+        : [0]
     return { labels, serieValues }
 }
 
