4ac9de8f3ed9bf271d31e30cf30a3fa2f03732ea
Amine Belmahi <amine@myem.fr>
2023-12-10 17:27:54 +0100

    MYEM-5455 #time 3h #comment Fix nrlinkPowerData export and update nrlinkPowerHook

--- src/mocks/handlers/dashboard.ts
+++ src/mocks/handlers/dashboard.ts
@@ -3,7 +3,10 @@ import { API_RESOURCES_URL } from 'src/configs'
 import { INrlinkMetrics } from 'src/modules/Dashboard/StatusWrapper/nrlinkPower'
 import { SnakeCasedPropertiesDeep } from 'type-fest'
 
-const nrlinkPowerData: SnakeCasedPropertiesDeep<INrlinkMetrics> = {
+// export const SUCCESS_NRLINK_POWER_DATA =
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+export const nrlinkPowerData: SnakeCasedPropertiesDeep<INrlinkMetrics> = {
     last_power: {
         value: -55,
         timestamp: '2023-12-15T14:07:38.138000',
@@ -20,7 +23,7 @@ const nrlinkPowerData: SnakeCasedPropertiesDeep<INrlinkMetrics> = {
 export const DashboardEndpoints = [
     rest.get(`${API_RESOURCES_URL}/nrlink-last-metrics/:housingId`, (req, res, ctx) => {
         const { housingId } = req.params
-        if (housingId === 'error' || !housingId) {
+        if (housingId === -1) {
             return res(ctx.delay(1000), ctx.status(400))
         } else {
             return res(ctx.delay(1000), ctx.status(200), ctx.json(nrlinkPowerData))
