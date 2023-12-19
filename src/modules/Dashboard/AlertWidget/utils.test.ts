effe803a190d6fdd6d91fbe36028d07b5591a93b
Abdelkadir Bouziane <abdelkadir@myem.fr>
2023-12-19 19:19:01 +0100

    MYEM-5560 #comment Adjust utils tests

--- src/modules/Dashboard/AlertWidget/utils.test.ts
+++ src/modules/Dashboard/AlertWidget/utils.test.ts
@@ -3,25 +3,39 @@ import { AlertTypeEnum } from 'src/modules/Dashboard/AlertWidget/AlertWidget.d'
 
 describe('calculateGaugeChartPercent', () => {
     test('should return 1 when alertThreshold is less than or equal to 0 or when alertThreshold is greater than currentValue', () => {
-        expect(calculateGaugeChartPercent(-1, 10)).toBe(1)
-        expect(calculateGaugeChartPercent(0, 10)).toBe(1)
-        expect(calculateGaugeChartPercent(2, 4)).toBe(1)
-        expect(calculateGaugeChartPercent(5, 20)).toBe(1)
+        const testCases = [
+            { alertThreshold: -1, currentValue: 10, expectedPercent: 1 },
+            { alertThreshold: 0, currentValue: 12, expectedPercent: 1 },
+            { alertThreshold: 2, currentValue: 4, expectedPercent: 1 },
+        ]
+        testCases.forEach(({ alertThreshold, currentValue, expectedPercent }) => {
+            expect(calculateGaugeChartPercent(alertThreshold, currentValue)).toBe(expectedPercent)
+        })
     })
 
     test('should calculate the correct percent prop value', () => {
-        expect(calculateGaugeChartPercent(50, 25)).toBe(0.5)
-        expect(calculateGaugeChartPercent(100, 75)).toBe(0.75)
-        expect(calculateGaugeChartPercent(70, 15)).toBe(0.21)
+        const testCases = [
+            { alertThreshold: 50, currentValue: 25, expectedPercent: 0.5 },
+            { alertThreshold: 100, currentValue: 75, expectedPercent: 0.75 },
+            { alertThreshold: 70, currentValue: 15, expectedPercent: 0.21 },
+        ]
+        testCases.forEach(({ alertThreshold, currentValue, expectedPercent }) => {
+            expect(calculateGaugeChartPercent(alertThreshold, currentValue)).toBe(expectedPercent)
+        })
     })
 })
 
 describe('getFormatedAlertThreshold', () => {
     test('should return the correct formatted alertThreshold value for each AlertTypeEnum', () => {
-        expect(getFormatedAlertThreshold(50, AlertTypeEnum.PRICE)).toBe('50 €')
-        expect(getFormatedAlertThreshold(3.178, AlertTypeEnum.PRICE)).toBe('3.18 €')
-        expect(getFormatedAlertThreshold(120, AlertTypeEnum.CONSUMPTION)).toBe('120 Wh')
-        expect(getFormatedAlertThreshold(3041, AlertTypeEnum.CONSUMPTION)).toBe('3.04 kWh')
-        expect(getFormatedAlertThreshold(10, 'invalidType' as AlertTypeEnum)).toBe('')
+        const testCases = [
+            { alertThreshold: 50, alertType: AlertTypeEnum.PRICE, expectedAlertThreshold: '50 €' },
+            { alertThreshold: 3.178, alertType: AlertTypeEnum.PRICE, expectedAlertThreshold: '3.18 €' },
+            { alertThreshold: 120, alertType: AlertTypeEnum.CONSUMPTION, expectedAlertThreshold: '120 Wh' },
+            { alertThreshold: 3041, alertType: AlertTypeEnum.CONSUMPTION, expectedAlertThreshold: '3.04 kWh' },
+            { alertThreshold: 10, alertType: 'invalidType' as AlertTypeEnum, expectedAlertThreshold: '' },
+        ]
+        testCases.forEach(({ alertThreshold, alertType, expectedAlertThreshold }) => {
+            expect(getFormatedAlertThreshold(alertThreshold, alertType)).toBe(expectedAlertThreshold)
+        })
     })
 })
