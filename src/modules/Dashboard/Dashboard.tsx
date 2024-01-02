793432613181f61a009f193d50e55d51033e3f61
Fouad CHENNOUF <fouad@myem.fr>
2024-01-02 20:45:38 +0100

    #time 1h correction of dashboard page when no meter guid or no consents

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -1,5 +1,9 @@
 import PageSimple from 'src/common/ui-kit/fuse/components/PageSimple/PageSimple'
 import { DashboardContainer } from 'src/modules/Dashboard/DashboardContainer'
+import { useSelector } from 'react-redux'
+import { RootState } from 'src/redux'
+import { MissingHousingMeterErrorMessage } from 'src/modules/MyConsumption/utils/ErrorMessages'
+import { useConsents } from 'src/modules/Consents/consentsHook'
 
 /**
  * Dashboard page.
@@ -7,5 +11,11 @@ import { DashboardContainer } from 'src/modules/Dashboard/DashboardContainer'
  * @returns Dashboard.
  */
 export const Dashboard = () => {
+    const { currentHousing } = useSelector(({ housingModel }: RootState) => housingModel)
+    const { nrlinkConsent, enedisSgeConsent } = useConsents()
+    const nrlinkOff = nrlinkConsent?.nrlinkConsentState === 'NONEXISTENT'
+    const enedisOff = enedisSgeConsent?.enedisSgeConsentState !== 'CONNECTED'
+
+    if (!currentHousing?.meter?.guid || (nrlinkOff && enedisOff)) return <MissingHousingMeterErrorMessage />
     return <PageSimple content={<DashboardContainer />} />
 }
