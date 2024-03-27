2b767a06c0a72d341d59daaaeddefe18177219f3
Zakaria <zakaria@myem.fr>
2024-03-27 20:08:55 +0100

    MYEM-6340 #time 1d #comment add useGetShowNrLinkPopupHook and refactor code

---
+++ src/modules/nrLinkConnection/NrLinkConnectionHook.ts
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { axios } from 'src/common/react-platform-components'
+import { GET_SHOW_NRLINK_POPUP_ENDPOINT } from 'src/modules/nrLinkConnection/NrLinkConnection'
+
+/**
+ * Hook to get ShowNrLink Popup.
+ *
+ * @returns Get ShowNrLink Popup request handler.
+ */
+export const useGetShowNrLinkPopupHook = () => {
+    const initialMount = useRef(true)
+    const [isNrLinkPopupShowing, setIsNrLinkPopupShowing] = useState<boolean | null>(null)
+    const [isGetShowNrLinkLoading, setIsGetShowNrLinkLoading] = useState(false)
+
+    /**
+     * Get ShowNrLink Popup request handler.
+     */
+    const getShowNrLinkPopup = useCallback(async () => {
+        try {
+            setIsGetShowNrLinkLoading(true)
+            // eslint-disable-next-line jsdoc/require-jsdoc
+            const { data: responseData } = await axios.get<{ showNrlinkPopup: boolean }>(
+                `${GET_SHOW_NRLINK_POPUP_ENDPOINT}`,
+            )
+            setIsNrLinkPopupShowing(responseData.showNrlinkPopup)
+        } catch (error) {
+            setIsNrLinkPopupShowing(false)
+        } finally {
+            setIsGetShowNrLinkLoading(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (initialMount.current) {
+            initialMount.current = false
+            getShowNrLinkPopup()
+        }
+    }, [getShowNrLinkPopup])
+
+    return { isGetShowNrLinkLoading, isNrLinkPopupShowing }
+}
