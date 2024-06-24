2c6761d70677a9c9cab64656beca9e68576e076a
Amine Belmahi <amine@myem.fr>
2024-06-24 16:55:22 +0100

    Enable faq

--- src/modules/MyConsumption/components/ChartFAQ/ChartFAQ.test.tsx
+++ src/modules/MyConsumption/components/ChartFAQ/ChartFAQ.test.tsx
@@ -2,20 +2,17 @@ import { reduxedRender } from 'src/common/react-platform-components/test'
 import { PeriodEnum } from 'src/modules/MyConsumption/myConsumptionTypes.d'
 import { ChartFAQ } from 'src/modules/MyConsumption/components/ChartFAQ'
 import {
-    faqForDailyPeriod,
-    // faqForDailyPeriodTempo,
-    // faqForPeriodicIntervals,
-    // faqForPeriodicIntervalsTempo,
-    faqTitleForDailyPeriod,
-    // faqTitleForDailyPeriodTempo,
-    // faqTitleForPeriodicIntervals,
-    // faqTitleForPeriodicIntervalsTempo,
+    faqForDailyPeriodTempo,
+    faqForPeriodicIntervals,
+    faqTitleForDailyPeriodTempo,
+    faqTitleForPeriodicIntervals,
 } from 'src/modules/MyConsumption/components/ChartFAQ/ChartFAQVariables'
 import { TEST_DATETIME } from 'src/mocks/handlers/contracts'
 import { TEST_OFFERS } from 'src/mocks/handlers/commercialOffer'
 import { TEST_PROVIDERS } from 'src/mocks/handlers/commercialOffer'
 import { TEST_CONTRACT_TYPES } from 'src/mocks/handlers/commercialOffer'
 import { SwitchConsumptionButtonTypeEnum } from 'src/modules/MyConsumption/components/SwitchConsumptionButton/SwitchConsumptionButton.types'
+import { Matcher } from '@testing-library/react'
 
 let mockContracts = [
     {
@@ -50,13 +47,6 @@ jest.mock('src/modules/MyConsumption/store/myConsumptionStore', () => ({
 }))
 
 describe('ChartFAQ', () => {
-    test('renders FAQ component with correct props for daily period', () => {
-        const { getByText } = reduxedRender(<ChartFAQ period={PeriodEnum.DAILY} housingId={1} />)
-        expect(getByText(faqTitleForDailyPeriod.props.id)).toBeInTheDocument()
-        for (const faqItem of faqForDailyPeriod) {
-            expect(getByText((faqItem.title as JSX.Element).props.id)).toBeInTheDocument()
-        }
-    })
     test.each([PeriodEnum.WEEKLY, PeriodEnum.MONTHLY, PeriodEnum.YEARLY])(
         'should not shown on $period period.',
         (period) => {
@@ -69,45 +59,32 @@ describe('ChartFAQ', () => {
         const { queryByTestId } = reduxedRender(<ChartFAQ period={PeriodEnum.DAILY} housingId={2} />)
         expect(queryByTestId('faq')).not.toBeInTheDocument()
     })
-    test('should FAQ component not be not shown on tempo contract', () => {
+    test('should FAQ component not be shown on tempo contract', () => {
         mockContracts[0].tariffType.name = 'Jour Tempo'
         mockMyConsumptionTab = SwitchConsumptionButtonTypeEnum.Consumption
         const { queryByTestId } = reduxedRender(<ChartFAQ period={PeriodEnum.DAILY} housingId={2} />)
-        expect(queryByTestId('faq')).not.toBeInTheDocument()
+        expect(queryByTestId('faq')).toBeInTheDocument()
     })
     // todo: To activate the following tests later when faq content available.
-    // test('when user has tempo contract, renders the correct FAQ items for daily period', () => {
-    //     const { getByText } = render(<ChartFAQ period={PeriodEnum.DAILY} hasTempoContract={true} />)
+    test('when user has tempo contract, renders the correct FAQ items for daily period', () => {
+        const { getByText } = reduxedRender(<ChartFAQ period={PeriodEnum.DAILY} housingId={2} />)
 
-    //     expect(getByText(faqTitleForDailyPeriodTempo)).toBeInTheDocument()
-    //     for (const faqItem of faqForDailyPeriodTempo) {
-    //         expect(getByText(faqItem.title)).toBeInTheDocument()
-    //         expect(getByText(faqItem.content)).toBeInTheDocument()
-    //     }
-    // })
+        expect(getByText(faqTitleForDailyPeriodTempo)).toBeInTheDocument()
+        for (const faqItem of faqForDailyPeriodTempo) {
+            expect(getByText(faqItem.title as Matcher)).toBeInTheDocument()
+            expect(getByText(faqItem.content as Matcher)).toBeInTheDocument()
+        }
+    })
 
-    // test('renders FAQ component with correct props for periodic intervals', () => {
-    //     for (const period of [PeriodEnum.MONTHLY, PeriodEnum.WEEKLY]) {
-    //         const { getByText, unmount } = render(<ChartFAQ period={period} hasTempoContract={false} />)
-    //         expect(getByText(faqTitleForPeriodicIntervals)).toBeInTheDocument()
+    test("should not render FAQ unless it's daily", () => {
+        for (const period of [PeriodEnum.MONTHLY, PeriodEnum.WEEKLY]) {
+            const { getByText } = reduxedRender(<ChartFAQ period={period} housingId={2} />)
+            expect(() => getByText(faqTitleForPeriodicIntervals)).toThrow()
 
-    //         for (const faqItem of faqForPeriodicIntervals) {
-    //             expect(getByText(faqItem.title)).toBeInTheDocument()
-    //             expect(getByText(faqItem.content)).toBeInTheDocument()
-    //         }
-    //         unmount()
-    //     }
-    // })
-
-    // test('when user has tempo contract, renders the correct FAQ items for periodic intervals', () => {
-    //     for (const period of [PeriodEnum.MONTHLY, PeriodEnum.WEEKLY]) {
-    //         const { getByText, unmount } = render(<ChartFAQ period={period} hasTempoContract={true} />)
-    //         expect(getByText(faqTitleForPeriodicIntervalsTempo)).toBeInTheDocument()
-    //         for (const faqItem of faqForPeriodicIntervalsTempo) {
-    //             expect(getByText(faqItem.title)).toBeInTheDocument()
-    //             expect(getByText(faqItem.content)).toBeInTheDocument()
-    //         }
-    //         unmount()
-    //     }
-    // })
+            for (const faqItem of faqForPeriodicIntervals) {
+                expect(() => getByText(faqItem.title as Matcher)).toThrow()
+                expect(() => getByText(faqItem.content as Matcher)).toThrow()
+            }
+        }
+    })
 })
